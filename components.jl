173fb108e8006f50bbd33543c3f01a3772694120
Martijn Visser <mgvisser@gmail.com>
2022-03-07 22:32:49 +0100

    split up connectors, remove water types
    The water types were nice, but they did slow down development, and add a lot of equations. Perhaps revisit later.

--- components.jl
+++ components.jl
@@ -5,81 +5,62 @@ using Symbolics: Symbolics, scalarize
 
 @parameters t
 
-@connector function FluidPort(; name, nwat = nothing, h0 = 0.0, Q0 = 0.0)
-    if isnothing(nwat)
-        nwat = max(length(h0), length(Q0))
-    end
-    length(h0) in (1, nwat) || DimensionMismatch("h0 should be length 1 or nwat")
-    length(Q0) in (1, nwat) || DimensionMismatch("Q0 should be length 1 or nwat")
-    # h [m]: hydraulic head
-    # Q [m3 s⁻¹]: volumetric flux
-    @variables h[1:nwat](t) = h0 Q[1:nwat](t) = Q0 [connect = Flow]
-    ODESystem(Equation[], t, [h..., Q...], []; name)
+"h [m]: hydraulic head above reference level"
+@connector function Head(; name, h0 = 0.0)
+    vars = @variables h(t) = h0
+    ODESystem(Equation[], t, vars, []; name)
 end
 
-function Bucket(; name, s0, α, β, bottom, k)
-    # do we need h0 here, given s0?
-    nwat = length(s0)
-    @named i = FluidPort(; nwat)
-    @named o = FluidPort(; nwat)
-    pars = @parameters α = α β = β bottom = bottom
-    # h [m]: hydraulic head above (s=0 and Q=0) bottom
-    # s [m3]: storage
-    @variables s[1:nwat](t) = s0 h(t)
+"S [m³]: storage"
+@connector function Storage(; name, S0 = 0.0)
+    vars = @variables S(t) = S0
+    ODESystem(Equation[], t, vars, []; name)
+end
+
+"Q [m3 s⁻¹]: volumetric flux"
+@connector function Discharge(; name, Q0 = 0.0)
+    vars = @variables Q(t) = Q0 [connect = Flow]
+    ODESystem(Equation[], t, vars, []; name)
+end
+
+function Bucket(; name, S0, α, β, k)
+    @named storage = Storage(; S0)
+    @named x = Discharge()
+    @named o = Discharge()
+    @named head = Head()
+    (; h) = head
+    (; S) = storage
+
+    pars = @parameters α = α β = β
     D = Differential(t)
 
     # scalarizing sums is not needed, but nicely writes them out
     eqs = Equation[
-        scalarize(h ~ sum(o.h) - bottom)
         # Q(h) rating curve
-        scalarize(-sum(o.Q) ~ α * h^β)
+        # TODO use h above bottom here, or max to avoid DomainError
+        # -o.Q ~ α * h^β
+        -o.Q ~ α * h
         # s(h) volume level
-        scalarize(sum(s) ~ k * h)
-        # full mixing of storage (what about dividing by 0?)
-        [scalarize(o.Q[w] / sum(o.Q) ~ s[w] / sum(s)) for w = 1:nwat-1]...
-        [scalarize(o.h[w] / sum(o.h) ~ s[w] / sum(s)) for w = 1:nwat-1]...
-        # storage / balance, per water type
-        # scalarize(D.(s) .~ i.Q .+ o.Q)...  # needs https://github.com/JuliaSymbolics/Symbolics.jl/pull/486
-        [D(s[w]) ~ i.Q[w] + o.Q[w] for w = 1:nwat]...
+        S ~ k * h
+        # storage / balance
+        D(S) ~ x.Q + o.Q
     ]
-    compose(ODESystem(eqs, t, [s...], pars; name), i, o)
-end
-
-function Darcy(; name, nwat, K, A, L)
-    @named a = FluidPort(; nwat)
-    @named b = FluidPort(; nwat)
-    # aggregated h and Q
-    vars = @variables h(t) Q(t)
-    pars = @parameters K = K A = A L = L
-
-    eqs = Equation[
-        Q ~ sum(a.Q)
-        # negative gradient -> positive flow
-        h ~ sum(b.h) - sum(a.h)
-        # Darcy
-        scalarize(Q ~ -K * A * h / L)
-        # full mixing
-        [scalarize(a.Q[w] / Q ~ a.h[w] / sum(a.h)) for w = 1:nwat-1]...
-        # conservation of volume, per water type
-        scalarize(a.Q .+ b.Q .~ 0)...
-    ]
-    compose(ODESystem(eqs, t, vars, pars; name), a, b)
+    compose(ODESystem(eqs, t, [h...], pars; name), head, storage, x, o)
 end
 
 function ConstantHead(; name, h0)
-    @named o = FluidPort(; h0)
-    nwat = length(o.h)
-    @parameters h[1:nwat] = h0
+    @named head = Head(; h0)
+    @parameters h0 = h0
 
-    eqs = Equation[scalarize(o.h .~ h)...]
-    compose(ODESystem(eqs, t, [], [h...]; name), o)
+    eqs = Equation[head.h~h0]
+    compose(ODESystem(eqs, t, [], [h0...]; name), head)
 end
 
 function ConstantFlux(; name, Q0)
-    @named o = FluidPort(; Q0)
-    nwat = length(o.h)
-    @parameters Q[1:nwat] = Q0
+    @named x = Discharge(; Q0)
+    nwat = length(x.Q)
+    @parameters Q0[1:nwat] = Q0
 
-    eqs = Equation[scalarize(o.Q .~ Q)...]
-    compose(ODESystem(eqs, t, [], [Q...]; name), o)
+    eqs = Equation[scalarize(x.Q .~ Q0)...]
+    compose(ODESystem(eqs, t, [], [Q0...]; name), x)
 end
--- connect.jl
+++ connect.jl
@@ -14,36 +14,30 @@ using Random
 import Distributions
 
 includet("components.jl")
-# plot stacked storage
-function splot(x, y; labels = nothing, title = "", yflip = false)
-    Plots.areaplot(x, VectorOfArray(y); yflip, labels, title)
-end
-# plot stacked discharge, outflow is negative so flipped axis
-qplot(x, y; labels = nothing, title = "", yflip = true) = splot(x, y; labels, title, yflip)
 
-watnames = [:precip, :storage1, :storage2, :storage3]
-labels = permutedims(String.(watnames))
-nwat = length(watnames)
-
-@named inflow = ConstantFlux(Q0 = [-5.0, 0, 0, 0])
-@named bucket1 = Bucket(k = 20.0, α = 1.0e2, β = 1.5, bottom = 0.0, s0 = [0.0, 0.3, 0, 0])
-@named bucket2 = Bucket(k = 20.0, α = 1.0e2, β = 1.5, bottom = 0.0, s0 = [0.0, 0, 0.4, 0])
-@named bucket3 = Bucket(k = 20.0, α = 1.0e2, β = 1.5, bottom = 0.0, s0 = [0.0, 0, 0, 0.5])
-@named darcy = Darcy(; nwat, K = 0.7, A = 1.0, L = 1.0)
-@named head_in = ConstantHead(h0 = [1.0, 2, 2, 2])
-@named head = ConstantHead(h0 = [3.0, 3.5, 3.5, 3.5])
+# when adding water, you should be able to specify the type
+# but not when removing water
+@named inflow = ConstantFlux(Q0 = -5.0)
+# TODO make a user that is connected to S and/or h, which prevents negative storage
+@named user = ConstantFlux(Q0 = 4.0)
+@named bucket1 = Bucket(k = 20.0, α = 1.0e2, β = 1.5, S0 = 0.3)
+@named bucket2 = Bucket(k = 20.0, α = 1.0e2, β = 1.5, S0 = 0.4)
+@named bucket3 = Bucket(k = 20.0, α = 1.0e2, β = 1.5, S0 = 0.5)
+@named head_in = ConstantHead(h0 = 1.0)
+@named head = ConstantHead(h0 = 3.0)
 
 eqs = [
     # connect(head_in.o, darcy.a)
     # connect(darcy.b, head.o)
-    connect(inflow.o, bucket1.i)
-    connect(bucket1.o, bucket2.i)
-    connect(bucket2.o, bucket3.i)
+    connect(inflow.x, bucket1.x)
+    connect(bucket1.o, bucket2.x)
+    connect(user.x, bucket3.x)
+    connect(bucket2.o, bucket3.x)
 ]
 
 @named _sys = ODESystem(eqs, t)
 # @named sys = compose(_sys, [inflow, bucket1, bucket2])
-@named sys = compose(_sys, [inflow, bucket1, bucket2, bucket3])
+@named sys = compose(_sys, [inflow, user, bucket1, bucket2, bucket3])
 # @named sys = compose(_sys, [inflow, bucket1, darcy, head])
 # @named sys = compose(_sys, [head_in, darcy, head])
 sys
@@ -60,12 +54,7 @@ observed(sim)
 prob = ODEProblem(sim, [], (0, 1e0))
 sol = solve(prob)
 
-qplot(sol.t, sol[inflow.o.Q]; labels, title = "inflow")
-qplot(sol.t, sol[bucket1.o.Q]; labels, title = "bucket 1 discharge")
-qplot(sol.t, sol[bucket2.o.Q]; labels, title = "bucket 2 discharge")
-qplot(sol.t, sol[bucket3.o.Q]; labels, title = "bucket 3 discharge")
-
-splot(sol.t, sol[bucket1.s]; labels, title = "bucket 1 storage")
-splot(sol.t, sol[bucket2.s]; labels, title = "bucket 2 storage")
-splot(sol.t, sol[bucket3.s]; labels, title = "bucket 3 storage")
-
+Plots.plot(sol)
+# Plots.plot(sol, vars=[inflow.x.Q, bucket3.x.Q])
+# Plots.plot(sol, vars=[user.x.Q, bucket2.o.Q, bucket3.x.Q])
+# Plots.plot(sol, vars=[user.x.Q + bucket2.o.Q + bucket3.x.Q])
