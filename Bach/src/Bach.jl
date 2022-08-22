fcfbed3b9cac8fe7260406d36983c14262bf282c
Martijn Visser <mgvisser@gmail.com>
2022-08-22 17:20:43 +0200

    Initializing gh-pages branch

---
+++ Bach/src/Bach.jl
@@ -0,0 +1,18 @@
+module Bach
+
+using DiffEqCallbacks
+using DifferentialEquations
+using ModelingToolkit
+using QuadGK
+using SciMLBase
+using Symbolics: getname
+using DataFrames
+using DataFrameMacros
+using Dates
+
+export interpolator, Register, ForwardFill
+
+include("lib.jl")
+include("system.jl")
+
+end # module Bach
