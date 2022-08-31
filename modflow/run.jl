816402cb68fc4a196a6dccb97d6e992562a337fe
Huite Bootsma <huitebootsma@gmail.com>
2022-08-31 21:47:40 +0200

    Update run.jl with moved file (from Bach/src)

--- modflow/run.jl
+++ modflow/run.jl
@@ -1,13 +1,13 @@
 using TOML
-include("run-mf6.jl")
+include("../Bach/src/modflow.jl")
 import BasicModelInterface as BMI
 
 # Standard run
 cd("c:/src/bach/modflow")
 config = TOML.parsefile("c:/src/bach/modflow/couple.toml")
-bach_ids = [151358]#, 151371, 151309]
-bach = BachModel()#BMI.initialize(Bach.Register, config)
-bme = BachModflowExchange(config, bach_ids);
+#bach_ids = [151358]#, 151371, 151309]
+bach_ids = [200164]
+bme = BachModflowExchange(config["modflow6"], bach_ids);
 start_time = BMI.get_start_time(bme.modflow.bmi)
 current_time = BMI.get_current_time(bme.modflow.bmi)
 end_time = BMI.get_end_time(bme.modflow.bmi)
