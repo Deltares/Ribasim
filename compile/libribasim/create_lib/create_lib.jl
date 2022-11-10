6b2510dd6a43633223e8d61f3f87a82f43597927
Martijn Visser <mgvisser@gmail.com>
2022-11-10 12:43:40 +0100

    libribasim: lazy artifacts are not needed
    This reduces the size of the bundle from 2GB to 1GB.

--- compile/libribasim/create_lib/create_lib.jl
+++ compile/libribasim/create_lib/create_lib.jl
@@ -5,4 +5,4 @@ cd(@__DIR__)
 create_library("..", "libribasim";
                lib_name = "libribasim",
                precompile_execution_file = "precompile.jl",
-               include_lazy_artifacts = true)
+               include_lazy_artifacts = false)
