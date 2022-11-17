a16a3b666132c457802541b44f21cd18949c2e42
Martijn Visser <mgvisser@gmail.com>
2022-11-17 14:17:34 +0100

    download test data if needed
    Still needs improvement, as this does not check if the contents of the hosted files have changed.

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,4 +1,30 @@
-using Ribasim, Test, SafeTestsets, Aqua
+using Ribasim, Test, SafeTestsets, Aqua, Downloads
+
+# ensure test data is present
+testdir = @__DIR__
+datadir = joinpath(testdir, "data")
+isdir(datadir) || mkpath(datadir)
+
+"Download a test data file if it does not already exist"
+function testdata(source_filename, target_filename=source_filename)
+    target_path = joinpath(datadir, target_filename)
+    base_url = "https://github.com/visr/ribasim-artifacts/releases/download/v0.1.0/"
+    url = string(base_url, source_filename)
+    isfile(target_path) || Downloads.download(url, target_path)
+    return target_path
+end
+
+testdata("dummyforcing_151358_P.csv")
+testdata("dummyforcing_151358_V.csv")
+
+# a schematization for all of the Netherlands
+lhmdir = joinpath(datadir, "lhm")
+isdir(lhmdir) || mkpath(lhmdir)
+testdata("forcing.arrow", "lhm/forcing.arrow")
+testdata("network.ply", "lhm/network.ply")
+testdata("profile.arrow", "lhm/profile.arrow")
+testdata("state.arrow", "lhm/state.arrow")
+testdata("static.arrow", "lhm/static.arrow")
 
 @testset "Ribasim" begin
     @safetestset "Input/Output" begin include("io.jl") end
