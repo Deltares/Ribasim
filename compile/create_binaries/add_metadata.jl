299cd55240bae2ca2961e78555aea13f3870b840
Hofer-Julian <julianhofer@gnome.org>
2023-02-14 10:44:44 +0100

    Finish up compile restructuring

--- compile/create_binaries/add_metadata.jl
+++ compile/create_binaries/add_metadata.jl
@@ -1,22 +1,3 @@
-using PackageCompiler
-using TOML
-using LibGit2
-
-# change directory to this script's location
-cd(@__DIR__)
-
-appdir = "./ribasim_cli"
-
-create_app(
-    "..",
-    appdir;
-    # map from binary name to julia function name
-    executables = ["ribasim" => "julia_main"],
-    precompile_execution_file = "precompile.jl",
-    filter_stdlibs = false,
-    force = true,
-)
-
 """
 Add the following metadata files to the newly created build:
 
@@ -26,34 +7,42 @@ Add the following metadata files to the newly created build:
 - README.md
 - LICENSE
 """
-function add_metadata()
+function add_metadata(project_dir, license_file, output_dir, git_repo)
     # save some environment variables in a Build.toml file for debugging purposes
     vars = ["BUILD_NUMBER", "BUILD_VCS_NUMBER"]
     dict = Dict(var => ENV[var] for var in vars if haskey(ENV, var))
-    open(normpath(appdir, "share/julia/Build.toml"), "w") do io
+    open(normpath(output_dir, "share/julia/Build.toml"), "w") do io
         TOML.print(io, dict)
     end
 
     # a stripped Project.toml is already added in the same location by PackageCompiler
     # however it is better to copy the original, since it includes the version and compat
-    cp("../Project.toml", normpath(appdir, "share/julia/Project.toml"); force = true)
+    cp(
+        normpath(project_dir, "Project.toml"),
+        normpath(output_dir, "share/julia/Project.toml");
+        force = true,
+    )
     # the Manifest.toml always gives the exact version of Ribasim that was built
-    cp("../Manifest.toml", normpath(appdir, "share/julia/Manifest.toml"); force = true)
+    cp(
+        normpath(project_dir, "Manifest.toml"),
+        normpath(output_dir, "share/julia/Manifest.toml");
+        force = true,
+    )
 
     # put the LICENSE in the top level directory
-    cp("../../../LICENSE", normpath(appdir, "LICENSE"); force = true)
-    cp("../README.md", normpath(appdir, "README.md"); force = true)
-    open(normpath(appdir, "README.md"), "a") do io
+    cp(license_file, normpath(output_dir, "LICENSE"); force = true)
+    cp(normpath(project_dir, "README.md"), normpath(output_dir, "README.md"); force = true)
+    open(normpath(output_dir, "README.md"), "a") do io
         # since the exact Ribasim version may be hard to find in the Manifest.toml file
         # we can also extract that information, and add it to the README.md
-        manifest = TOML.parsefile("../Manifest.toml")
+        manifest = TOML.parsefile(normpath(project_dir, "Manifest.toml"))
         if !haskey(manifest, "manifest_format")
             error("Manifest.toml is in the old format, run Pkg.upgrade_manifest()")
         end
         julia_version = manifest["julia_version"]
         ribasim_entry = only(manifest["deps"]["Ribasim"])
         version = ribasim_entry["version"]
-        repo = GitRepo("../../..")
+        repo = GitRepo(git_repo)
         branch = LibGit2.head(repo)
         commit = LibGit2.peel(LibGit2.GitCommit, branch)
         short_name = LibGit2.shortname(branch)
@@ -74,5 +63,3 @@ function add_metadata()
         println(io, version_info)
     end
 end
-
-add_metadata()
--- compile/ribasim_cli/create_app/precompile.jl
+++
@@ -1,6 +0,0 @@
-# this script is run during app creation to precompile the code executed in this script
-# that way the app will have less latency
-
-using Ribasim
-
-Ribasim.run("../../../run/run.toml")
