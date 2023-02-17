847592ed9801177c2c2081451dfb7554d2a2b234
Maarten Pronk <git@evetion.nl>
2023-02-17 11:00:08 +0100

    Address review comments.

--- test/config.jl
+++ test/config.jl
@@ -1,5 +1,6 @@
 using Ribasim
 using Dates
+using Configurations
 
 @testset "config" begin
     config = Ribasim.parsefile(joinpath(@__DIR__, "testrun.toml"))
@@ -7,5 +8,12 @@ using Dates
     @test config.update_timestep == 86400.0
     @test config.endtime > config.starttime
 
+    @test_throws Configurations.UndefKeywordError Ribasim.Config()
+    @test_throws Configurations.UndefKeywordError Ribasim.Config(
+        startime = now(),
+        endtime = now(),
+        geopackage = "",
+        foo = "bar",
+    )
     # not sure what else is useful here given that model will fail anyway if this test fails
 end
