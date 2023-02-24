c8373a0c889aca9d2faceb4e23b07b072fd8f891
Martijn Visser <mgvisser@gmail.com>
2023-02-24 09:37:47 +0100

    fix build; use the testrun.toml for precompilation
    This is more robust since it is also covered by tests.

--- compile/create_binaries/precompile.jl
+++ compile/create_binaries/precompile.jl
@@ -8,4 +8,4 @@ include("../../utils/testdata.jl")
 testdata("model.gpkg", normpath(@__DIR__, "../../data/lhm/model.gpkg"))
 testdata("forcing.arrow", normpath(@__DIR__, "../../data/lhm/forcing.arrow"))
 
-Ribasim.run("../../run/run.toml")
+Ribasim.run("../../test/testrun.toml")
