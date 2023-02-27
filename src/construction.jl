33c314e90de6b4b862c5fd11389a0a770f3ef8ad
Huite Bootsma <huitebootsma@gmail.com>
2023-02-27 17:06:45 +0100

    Use initial state, make transient forcing work

--- src/construction.jl
+++ src/construction.jl
@@ -20,6 +20,21 @@ function load_data(db::DB, config::Config, tablename::String)::Union{Table, Quer
     return nothing
 end
 
+
+function load_dataframe(db::DB, config::Config, tablename::String)::Union{DataFrame, Nothing}
+    query = load_data(db, config, tablename)
+    if isnothing(query)
+        return nothing
+    end
+
+    df = DataFrame(query)
+    if hasproperty(df, :time)
+        df.time = DateTime.(df.time)
+    end
+    return df
+end
+
+
 function load_required_data(
     db::DB,
     config::Config,
