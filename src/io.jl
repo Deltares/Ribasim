c24fdd17d5ca8d67871a034a079e9d37e506c02c
Martijn Visser <mgvisser@gmail.com>
2023-03-02 21:14:45 +0100

    use simulation starttime as t = 0, not 1970
    Internally we need to represent time as a floating point number. That means we need a method to convert from DateTime to this float and vice-versa. So far we used Unix time, seconds since 1970. This was convenient at first since there was a global datum.
    
    There were several downsides to using unix time however. First of all, the floats were hard to read when debugging: `datetime2unix(now()) == 1.677761097515e9`. The precision is enough for Float64 (`eps(ans)
    2.384185791015625e-7`), but if we want to move to Float32 it could give issues, especially when moving further away from 1970.
    
    So instead this maps the starttime to 0.0. This will have more precision, and to avoid hard to debug accuracy errors we error out if `eps(t_end)` is higher than 1 hour. We need the `config.starttime` to convert, but usually have Config available anyway. I put the Config in the Register struct as well to be able to map times after running a simulation as well.
    
    Note: I considered two other options. Option 1 was to always run from 0 to 1, but not having a fixed time unit is inconvenient, and not convenient for the BMI for instance. I also considered using days since starttime, but then you need to do `du .*= 86400` to get `du` right. which doesn't feel great.
    
    BMI also states most models start time at 0.0: https://bmi.readthedocs.io/en/stable/#get-start-time

--- src/io.jl
+++ src/io.jl
@@ -29,12 +29,29 @@ function split_tablename(tablename)
     return Symbol(nodetype), Symbol(kind)
 end
 
-function write_basin_output(reg::Register, config::Config)
-    (; sol, p) = reg.integrator
+"""
+    seconds_since(t::DateTime, t0::DateTime)::Float64
+
+Convert a DateTime to a float that is the number of seconds since the start of the
+simulation. This is used to convert between the solver's inner float time, and the calendar.
+"""
+seconds_since(t::DateTime, t0::DateTime)::Float64 = 0.001 * Dates.value(t - t0)
+
+"""
+    time_since(t::Real, t0::DateTime)::DateTime
+
+Convert a Real that represents the seconds passed since the simulation start to the nearest
+DateTime. This is used to convert between the solver's inner float time, and the calendar.
+"""
+time_since(t::Real, t0::DateTime)::DateTime = t0 + Millisecond(round(1000 * t))
+
+function write_basin_output(reg::Register)
+    (; config, integrator) = reg
+    (; sol, p) = integrator
 
     basin_id = collect(keys(p.connectivity.u_index))
     nbasin = length(basin_id)
-    tsteps = unix2datetime.(timesteps(reg))
+    tsteps = time_since.(timesteps(reg), config.starttime)
     ntsteps = length(tsteps)
 
     time = convert.(Arrow.DATETIME, repeat(tsteps; inner = nbasin))
@@ -52,15 +69,16 @@ function write_basin_output(reg::Register, config::Config)
     Arrow.write(path, basin; compress = :lz4)
 end
 
-function write_flow_output(reg::Register, config::Config)
-    (; t, saveval) = reg.saved_flow
-    (; connectivity) = reg.integrator.p
+function write_flow_output(reg::Register)
+    (; config, saved_flow, integrator) = reg
+    (; t, saveval) = saved_flow
+    (; connectivity) = integrator.p
 
     I, J, _ = findnz(connectivity.flow)
     nflow = length(I)
     ntsteps = length(t)
 
-    time = convert.(Arrow.DATETIME, repeat(unix2datetime.(t); inner = nflow))
+    time = convert.(Arrow.DATETIME, repeat(time_since.(t, config.starttime); inner = nflow))
     from_node_id = repeat(I; outer = ntsteps)
     to_node_id = repeat(J; outer = ntsteps)
     flow = collect(Iterators.flatten(saveval))
