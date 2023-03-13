16f74f83d5f711861637ca6bfea51246f6fd9709
Hofer-Julian <julianhofer@gnome.org>
2023-03-13 15:37:00 +0100

    Add first part of toml_path test

--- core/test/cli.jl
+++ core/test/cli.jl
@@ -23,3 +23,15 @@ end
     @test output == string(Ribasim.pkgversion(Ribasim))
     empty!(Base.ARGS)
 end
+
+@testset "toml_path" begin
+    toml_path = normpath(@__DIR__, "../../data/basic/basic.toml")
+    @test ispath(toml_path)
+    empty!(Base.ARGS)
+    push!(Base.ARGS, toml_path)
+    result, output = @capture_stdout(ribasim_cli.julia_main())
+    @test result == 0
+    # TODO
+    #@test model.integrator.sol.u[end] ≈ Float32[187.27687, 138.03664, 122.17141, 1504.5299]
+    empty!(Base.ARGS)
+end
