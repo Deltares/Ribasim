db389594d960dc6ac714f8464a545f4d96643d2b
Hofer-Julian <julianhofer@gnome.org>
2023-03-15 10:43:38 +0100

    Refactor Ribasim Python
    Increase Python version
    
    Setup shell for mamba
    
    Use multiple Python versions
    
    Add Python lint
    
    Limit CI to paths
    
    Also run Python jobs if .github is modified

---
+++ python/README.md
@@ -0,0 +1,20 @@
+# Ribasim Python
+
+The Ribasim Python package (named `ribasim`) aims to make it easy to build, update and analyze Ribasim models
+programmatically.
+
+The Ribasim QGIS plugin allows users to construct a model from scratch without programming.
+For specific tasks, like adding observed rainfall timeseries, it can be faster to use
+Python instead.
+
+One can also use Ribasim Python to build entire models from base data, such that your model
+setup is fully reproducible.
+
+The package is [registered in PyPI](https://pypi.org/project/ribasim/) and can therefore
+be installed with `pip install ribasim`.
+
+For documentation please see the [examples](https://deltares.github.io/Ribasim/python/examples.html) and [API reference](https://deltares.github.io/Ribasim/python/reference/).
+
+# Contributing
+
+For the developer docs please have a look at https://deltares.github.io/Ribasim/python/developer.html
--- python/ribasim/__init__.py
+++ python/ribasim/__init__.py
@@ -1,12 +1,14 @@
 __version__ = "0.1.1"
 
 
-from ribasim import utils
-from ribasim.basin import Basin
-from ribasim.edge import Edge
-from ribasim.fractional_flow import FractionalFlow
-from ribasim.level_control import LevelControl
-from ribasim.linear_level_connection import LinearLevelConnection
-from ribasim.model import Model
-from ribasim.node import Node
-from ribasim.tabulated_rating_curve import TabulatedRatingCurve
+from ribasim import utils as utils
+from ribasim.basin import Basin as Basin
+from ribasim.edge import Edge as Edge
+from ribasim.fractional_flow import FractionalFlow as FractionalFlow
+from ribasim.level_control import LevelControl as LevelControl
+from ribasim.linear_level_connection import (
+    LinearLevelConnection as LinearLevelConnection,
+)
+from ribasim.model import Model as Model
+from ribasim.node import Node as Node
+from ribasim.tabulated_rating_curve import TabulatedRatingCurve as TabulatedRatingCurve
