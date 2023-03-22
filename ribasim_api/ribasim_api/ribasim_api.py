5bd1cb58ac7ceb60b54e9a70555484a02f0bbfaf
Hofer-Julian <julianhofer@gnome.org>
2023-03-22 16:47:54 +0100

    Create Ribasim API

---
+++ ribasim_api/ribasim_api/ribasim_api.py
@@ -0,0 +1,29 @@
+# %%
+from ctypes import byref, c_int, create_string_buffer
+
+from xmipy import XmiWrapper
+
+
+class RibasimApi(XmiWrapper):
+    def get_constant_int(self, name: str) -> int:
+        match name:
+            case "BMI_LENVARTYPE":
+                return 51
+            case "BMI_LENGRIDTYPE":
+                return 17
+            case "BMI_LENVARADDRESS":
+                return 68
+            case "BMI_LENCOMPONENTNAME":
+                return 256
+            case "BMI_LENVERSION":
+                return 256
+            case "BMI_LENERRMESSAGE":
+                return 1025
+        raise ValueError(f"{name} does not map to an integer exposed by Ribasim")
+
+    def init_julia(self) -> None:
+        argument = create_string_buffer(0)
+        self.lib.init_julia(c_int(0), byref(argument))
+
+    def shutdown_julia(self) -> None:
+        self.lib.shutdown_julia(c_int(0))
