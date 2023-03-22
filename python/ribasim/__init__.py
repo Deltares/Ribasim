b18e44c46884a5d9d11e8273f135c1e5b501b6e2
Hofer-Julian <julianhofer@gnome.org>
2023-03-22 16:47:37 +0100

    Move part of Ribasim Python to root dir

--- python/ribasim/__init__.py
+++ python/ribasim/__init__.py
@@ -1,16 +1,25 @@
 __version__ = "0.1.1"
 
 
-from ribasim import utils as utils
-from ribasim.basin import Basin as Basin
-from ribasim.edge import Edge as Edge
-from ribasim.fractional_flow import FractionalFlow as FractionalFlow
-from ribasim.level_control import LevelControl as LevelControl
-from ribasim.linear_level_connection import (
-    LinearLevelConnection as LinearLevelConnection,
-)
-from ribasim.model import Model as Model
-from ribasim.model import Solver as Solver
-from ribasim.node import Node as Node
-from ribasim.pump import Pump as Pump
-from ribasim.tabulated_rating_curve import TabulatedRatingCurve as TabulatedRatingCurve
+from ribasim import utils
+from ribasim.edge import Edge
+from ribasim.fractional_flow import FractionalFlow
+from ribasim.level_control import LevelControl
+from ribasim.linear_level_connection import LinearLevelConnection
+from ribasim.model import Model, Solver
+from ribasim.node import Node
+from ribasim.pump import Pump
+from ribasim.tabulated_rating_curve import TabulatedRatingCurve
+
+__all__ = [
+    "utils",
+    "Edge",
+    "FractionalFlow",
+    "LevelControl",
+    "LinearLevelConnection",
+    "Model",
+    "Solver",
+    "Node",
+    "Pump",
+    "TabulatedRatingCurve",
+]
