480409f9f3468ae8f91869b9ce2c005a4aa0f15a
Hofer-Julian <julianhofer@gnome.org>
2023-03-24 11:00:27 +0100

    Fix libribasim tests
    Seems like I forgot to fix the tests after the last refactor

--- python/ribasim_api/tests/conftest.py
+++ python/ribasim_api/tests/conftest.py
@@ -5,29 +5,28 @@ from ribasim_api import RibasimApi
 
 
 @pytest.fixture(scope="session")
-def libribasim_paths() -> tuple[Path, Path]:
-    repo_root = Path(__file__).parents[2].resolve()
+def libribasim_paths() -> tuple[Path, Path, Path]:
+    repo_root = Path(__file__).parents[3].resolve()
     lib_folder = repo_root / "build" / "create_binaries" / "libribasim" / "bin"
     lib_path = lib_folder / "libribasim"
-    return lib_path, lib_folder
+    return repo_root, lib_path, lib_folder
 
 
 @pytest.fixture(scope="session", autouse=True)
 def load_julia(libribasim_paths) -> None:
-    lib_path, lib_folder = libribasim_paths
+    repo_root, lib_path, lib_folder = libribasim_paths
     libribasim = RibasimApi(lib_path, lib_folder)
     libribasim.init_julia()
 
 
 @pytest.fixture
 def ribasim_basic(libribasim_paths, request) -> tuple[RibasimApi, str]:
-    lib_path, lib_folder = libribasim_paths
+    repo_root, lib_path, lib_folder = libribasim_paths
     libribasim = RibasimApi(lib_path, lib_folder)
 
     # If initialized, call finalize() at end of use
     request.addfinalizer(libribasim.__del__)
 
-    repo_root = Path(__file__).parents[2].resolve()
     config_file = str(repo_root / "data" / "basic" / "basic.toml")
 
     return libribasim, config_file
