3362a1ed89250d59484724a1e44829671184b918
Martijn Visser <mgvisser@gmail.com>
2023-03-27 13:54:35 +0200

    add macro try_c to remove boilerplate

--- build/libribasim/src/libribasim.jl
+++ build/libribasim/src/libribasim.jl
@@ -5,58 +5,48 @@ using Ribasim
 
 model = nothing
 
+macro try_c(ex)
+    return quote
+        try
+            $(esc(ex))
+        catch
+            Base.invokelatest(Base.display_error, Base.catch_stack())
+            return 1
+        end
+        return 0
+    end
+end
+
 Base.@ccallable function initialize(path::Cstring)::Cint
-    global model
-    try
+    @try_c begin
         config_path = unsafe_string(path)
         model = BMI.initialize(Ribasim.Model, config_path)
-    catch
-        Base.invokelatest(Base.display_error, Base.catch_stack())
-        return 1
     end
-    return 0
 end
 
 Base.@ccallable function finalize()::Cint
-    global model
-    try
+    @try_c begin
         model = nothing
-    catch
-        Base.invokelatest(Base.display_error, Base.catch_stack())
-        return 1
     end
-    return 0
 end
 
 Base.@ccallable function update()::Cint
-    try
+    @try_c begin
         BMI.update(model)
-    catch
-        Base.invokelatest(Base.display_error, Base.catch_stack())
-        return 1
     end
-    return 0
 end
 
 Base.@ccallable function update_until(time::Cdouble)::Cint
-    try
+    @try_c begin
         BMI.update_until(model, time)
-    catch
-        Base.invokelatest(Base.display_error, Base.catch_stack())
-        return 1
     end
-    return 0
 end
 
 # TODO should receive a double that needs to get updated
 Base.@ccallable function get_current_time()::Cint
-    try
+    @try_c begin
         BMI.get_current_time(model)
-    catch
-        Base.invokelatest(Base.display_error, Base.catch_stack())
-        return 1
     end
-    return 0
 end
 
 end # module libribasim
