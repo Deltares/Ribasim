1f2785c1dad6253dcdb5b0022240b0ac6bbb096c
Hofer-Julian <julianhofer@gnome.org>
2023-03-27 15:59:20 +0200

    Add a test for `get_var_type`

--- python/ribasim_api/tests/test_bmi.py
+++ python/ribasim_api/tests/test_bmi.py
@@ -7,3 +7,10 @@ def test_update(ribasim_basic):
     libribasim, config_file = ribasim_basic
     libribasim.initialize(config_file)
     libribasim.update()
+
+
+def test_get_var_type(ribasim_basic):
+    libribasim, config_file = ribasim_basic
+    libribasim.initialize(config_file)
+    var_type = libribasim.get_var_type("volume")
+    assert var_type == "float64"
