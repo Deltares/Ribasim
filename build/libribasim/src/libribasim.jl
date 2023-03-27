1109aa03865c52255461b4f617d9d01c28f1c33b
Martijn Visser <mgvisser@gmail.com>
2023-03-27 15:27:38 +0200

    Add macro `try_c` to remove boilerplate (#139)

--- build/libribasim/src/libribasim.jl
+++ build/libribasim/src/libribasim.jl
@@ -5,58 +5,77 @@ using Ribasim
 
 model = nothing
 
-Base.@ccallable function initialize(path::Cstring)::Cint
+"""
+    @try_c(ex)
+
+The `try_c` macro adds boilerplate around the body of a C callable function.
+Specifically, it wraps the body in a try-catch,
+which always returns 0 on success and 1 on failure.
+On failure, it also prints the stacktrace.
+Also it makes the `model` from the global scope available.
+
+# Usage
+```
+@try_c begin
+    model = nothing
+end
+```
+
+This expands to
+```
+try
     global model
-    try
+    model = nothing
+catch
+    Base.invokelatest(Base.display_error, Base.catch_stack())
+    return 1
+end
+return 0
+```
+"""
+macro try_c(ex)
+    return quote
+        try
+            global model
+            $(esc(ex))
+        catch
+            Base.invokelatest(Base.display_error, Base.catch_stack())
+            return 1
+        end
+        return 0
+    end
+end
+
+Base.@ccallable function initialize(path::Cstring)::Cint
+    @try_c begin
         config_path = unsafe_string(path)
         model = BMI.initialize(Ribasim.Model, config_path)
-    catch
-        Base.invokelatest(Base.display_error, Base.catch_stack())
-        return 1
     end
-    return 0
 end
 
 Base.@ccallable function finalize()::Cint
-    global model
-    try
+    @try_c begin
         model = nothing
-    catch
-        Base.invokelatest(Base.display_error, Base.catch_stack())
-        return 1
     end
-    return 0
 end
 
 Base.@ccallable function update()::Cint
-    try
+    @try_c begin
         BMI.update(model)
-    catch
-        Base.invokelatest(Base.display_error, Base.catch_stack())
-        return 1
     end
-    return 0
 end
 
 Base.@ccallable function update_until(time::Cdouble)::Cint
-    try
+    @try_c begin
         BMI.update_until(model, time)
-    catch
-        Base.invokelatest(Base.display_error, Base.catch_stack())
-        return 1
     end
-    return 0
 end
 
 # TODO should receive a double that needs to get updated
 Base.@ccallable function get_current_time()::Cint
-    try
+    @try_c begin
         BMI.get_current_time(model)
-    catch
-        Base.invokelatest(Base.display_error, Base.catch_stack())
-        return 1
     end
-    return 0
 end
 
 end # module libribasim
