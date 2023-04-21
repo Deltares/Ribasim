ee05828b9db4fbe486781ef1632ae0cb7b2430fb
Martijn Visser <mgvisser@gmail.com>
2023-04-21 15:26:33 +0200

    don't use geopandas for nonspatial table IO
    Only the node and edge table support geometries. This copies the input_base IO functions to the node and edge classes, to special case them. These functions are the same for node and edge, so there is a bit of duplication there.
    
    In input_base we use the sqlite3 stdlib and pandas to read and write. I use `pd.read_sql_query` instead of `pd.read_sql_table` because that way it works with `sqlite3` stdlib instead of needing SQLAlchemy. Pandas refers to the sqlite3 functionality as legacy, but I thought it's not worth another dependency.
    
    Also fixes #173, by deleting old geopackage files if they exist.

--- python/ribasim/ribasim/input_base.py
+++ python/ribasim/ribasim/input_base.py
@@ -1,10 +1,9 @@
 import abc
 import textwrap
 from pathlib import Path
+from sqlite3 import Connection, connect
 from typing import Any, Dict, Type, TypeVar
 
-import fiona
-import geopandas as gpd
 import pandas as pd
 
 from ribasim.types import FilePath
@@ -14,6 +13,21 @@ T = TypeVar("T")
 __all__ = ("InputMixin",)
 
 delimiter = " / "
+
+
+def esc_id(identifier: str) -> str:
+    """Escape SQLite identifiers."""
+    return '"' + identifier.replace('"', '""') + '"'
+
+
+def exists(connection: Connection, name: str) -> bool:
+    """Check if a table exists in a SQLite database."""
+    cursor = connection.cursor()
+    cursor.execute(
+        "SELECT name FROM sqlite_master WHERE type='table' AND name=?", (name,)
+    )
+    result = cursor.fetchone()
+    return result is not None
 
 
 class InputMixin(abc.ABC):
@@ -74,29 +88,27 @@ class InputMixin(abc.ABC):
                 continue
             name = self._layername(field)
 
-            gdf = gpd.GeoDataFrame(data=dataframe)
-            if "geometry" in gdf.columns:
-                gdf = gdf.set_geometry("geometry")
-            else:
-                gdf["geometry"] = None
-            gdf.to_file(directory / f"{modelname}.gpkg", layer=name)
+            with connect(directory / f"{modelname}.gpkg") as connection:
+                dataframe.to_sql(name, connection, if_exists="replace")
 
         return
 
     @classmethod
     def _kwargs_from_geopackage(cls: Type[T], path: FilePath) -> Dict:
         kwargs = {}
-        layers = fiona.listlayers(path)
-        for key in cls.fields():
-            layername = cls._layername(key)
-            if layername in layers:
-                df = gpd.read_file(
-                    path, layer=layername, engine="pyogrio", fid_as_index=True
-                )
-            else:
-                df = None
+        with connect(path) as connection:
+            for key in cls.fields():
+                layername = cls._layername(key)
 
-            kwargs[key] = df
+                if exists(connection, layername):
+                    query = f"select * from {esc_id(layername)}"
+                    df = pd.read_sql_query(
+                        query, connection, index_col="index", parse_dates=["time"]
+                    )
+                else:
+                    df = None
+
+                kwargs[key] = df
 
         return kwargs
 
