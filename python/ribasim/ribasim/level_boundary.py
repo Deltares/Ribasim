60ccf7e5597871edde53abd8af5e3145cdbfebe2
Bart de Koning <bart123-4@hotmail.com>
2023-05-12 11:11:50 +0200

    Added level_boundary.py

---
+++ python/ribasim/ribasim/level_boundary.py
@@ -0,0 +1,36 @@
+import pandas as pd
+import pandera as pa
+from pandera.typing import Series
+from pydantic import BaseModel
+
+from ribasim.input_base import InputMixin
+
+__all__ = ("LevelBoundary",)
+
+
+class StaticSchema(pa.SchemaModel):
+    node_id: Series[int] = pa.Field(coerce=True)
+    level: Series[float]
+
+
+class LevelBoundary(InputMixin, BaseModel):
+    """
+    Stores water at a given level unaffected by flow, like an infinitely large basin.
+
+    Parameters
+    ----------
+    static: pandas.DataFrame
+
+        With columns:
+
+        * node_id
+        * level
+    """
+
+    _input_type = "Level"
+
+    class Config:
+        validate_assignment = True
+
+    def __init__(self, static: pd.DataFrame):
+        super().__init__(**locals())
