2950cb1c4be16e5c5fa4ca718da7f78806a1f018
Huite Bootsma <huitebootsma@gmail.com>
2023-06-19 19:21:40 +0200

    Rename test/basin.jl to run_models.jl

--- core/test/runtests.jl
+++ core/test/runtests.jl
@@ -7,7 +7,7 @@ using SafeTestsets: @safetestset
     @safetestset "Input/Output" include("io.jl")
     @safetestset "Configuration" include("config.jl")
     @safetestset "Equations" include("equations.jl")
-    @safetestset "Basin" include("basin.jl")
+    @safetestset "Run Test Models" include("run_models.jl")
     @safetestset "Basic Model Interface" include("bmi.jl")
     @safetestset "Command Line Interface" include("cli.jl")
     @safetestset "Utility functions" include("utils.jl")
