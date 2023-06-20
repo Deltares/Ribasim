Deltares/Ribasim

commit 1f9271dec2ff1fcb3daac5fda1757bf096a45614

Add Control tables to QGIS plugin and visualize control edge. (#335)
Fixes #301
Fixes #313 

We can't show the listen edges yet, but I would propose to fix that in a
seperate issue, by changing the control topology to make use of the edge
table.

Visualization of `data/pump_control/control.gpkg`
<img width="819" alt="Screenshot 2023-06-19 at 11 44 57"
src="https://github.com/Deltares/Ribasim/assets/8655030/55ecf215-9ac2-4e4d-bcdb-4dcad0594d63">

qgis/core/nodes.py
@@ -49,10 +49,20 @@ class Input(abc.ABC):
     Abstract base class for Ribasim input layers.
     """
 
+    input_type = ""
+
     def __init__(self, path: str):
         self.name = self.input_type
         self.path = path
         self.layer = None
+
+    @classmethod
+    def is_spatial(cls):
+        return False
+
+    @classmethod
+    def nodetype(cls):
+        return cls.input_type.split("/")[0].strip()
 
     @classmethod
     def create(cls, path: str, crs: Any, names: List[str]) -> "Input":
@@ -123,6 +133,10 @@ class Node(Input):
     geometry_type = "Point"
     attributes = (QgsField("type", QVariant.String),)
 
+    @classmethod
+    def is_spatial(cls):
+        return True
+
     def write(self) -> None:
         """
         Special case the Node layer write because it needs to generate a new
@@ -139,20 +153,7 @@ class Node(Input):
         index = layer.fields().indexFromName("type")
         setup = QgsEditorWidgetSetup(
             "ValueMap",
-            {
-                "map": {
-                    "Basin": "Basin",
-                    "FractionalFlow": "FractionalFlow",
-                    "TabulatedRatingCurve": "TabulatedRatingCurve",
-                    "LevelBoundary": "LevelBoundary",
-                    "FlowBoundary": "FlowBoundary",
-                    "LinearResistance": "LinearResistance",
-                    "ManningResistance": "ManningResistance",
-                    "Pump": "Pump",
-                    "Terminal": "Terminal",
-                    "Control": "Control",
-                },
-            },
+            {"map": {node: node for node in NONSPATIALNODETYPES}},
         )
         layer.setEditorWidgetSetup(index, setup)
 
@@ -222,33 +223,48 @@ class Edge(Input):
         QgsField("to_node_id", QVariant.Int),
     ]
 
+    @classmethod
+    def is_spatial(cls):
+        return True
+
     @property
-    def renderer(self) -> QgsSingleSymbolRenderer:
-        lightblue = "#3690c0"
+    def renderer(self) -> QgsCategorizedSymbolRenderer:
+        MARKERS = {
+            "flow": (QColor("#3690c0"), "flow"),  # lightblue
+            "control": (QColor("gray"), "control"),
+        }
 
-        # Create a line with an arrow marker to indicate directionality
-        arrow_marker = QgsSimpleMarkerSymbolLayer()
-        arrow_marker.setShape(QgsSimpleMarkerSymbolLayer.ArrowHeadFilled)
-        arrow_marker.setColor(QColor(lightblue))
-        arrow_marker.setSize(3)
-        arrow_marker.setStrokeStyle(Qt.PenStyle(Qt.NoPen))
+        categories = []
+        for value, (colour, label) in MARKERS.items():
+            # Create line
+            symbol = QgsLineSymbol()
+            symbol.setColor(QColor(colour))
+            symbol.setWidth(0.5)
 
-        marker_symbol = QgsMarkerSymbol()
-        marker_symbol.changeSymbolLayer(0, arrow_marker)
+            # Create an arrow marker to indicate directionality
+            arrow_marker = QgsSimpleMarkerSymbolLayer()
+            arrow_marker.setShape(QgsSimpleMarkerSymbolLayer.ArrowHeadFilled)
+            arrow_marker.setColor(QColor(colour))
+            arrow_marker.setSize(3)
+            arrow_marker.setStrokeStyle(Qt.PenStyle(Qt.NoPen))
 
-        marker_line_symbol_layer = QgsMarkerLineSymbolLayer.create(
-            {"placements": "SegmentCenter"}
+            # Add marker to line
+            marker_symbol = QgsMarkerSymbol()
+            marker_symbol.changeSymbolLayer(0, arrow_marker)
+            marker_line_symbol_layer = QgsMarkerLineSymbolLayer.create(
+                {"placements": "SegmentCenter"}
+            )
+            marker_line_symbol_layer.setSubSymbol(marker_symbol)
+            symbol.appendSymbolLayer(marker_line_symbol_layer)
+
+            category = QgsRendererCategory(value, symbol, label)
+            category.setRenderState(True)
+            categories.append(category)
+
+        renderer = QgsCategorizedSymbolRenderer(
+            attrName="edge_type", categories=categories
         )
-        marker_line_symbol_layer.setSubSymbol(marker_symbol)
-
-        line_symbol = QgsLineSymbol.createSimple(
-            {
-                "color": lightblue,
-                "width": "0.5",
-            }
-        )
-        line_symbol.appendSymbolLayer(marker_line_symbol_layer)
-        return QgsSingleSymbolRenderer(line_symbol)
+        return renderer
 
     def set_read_only(self) -> None:
         layer = self.layer
@@ -387,22 +403,30 @@ class FlowBoundaryStatic(Input):
     ]
 
 
-NODES = {
-    "Node": Node,
-    "Edge": Edge,
-    "Basin / static": BasinStatic,
-    "Basin / state": BasinState,
-    "Basin / profile": BasinProfile,
-    "Basin / forcing": BasinForcing,
-    "TabulatedRatingCurve / static": TabulatedRatingCurveStatic,
-    "TabulatedRatingCurve / time": TabulatedRatingCurveTime,
-    "FractionalFlow / static": FractionalFlowStatic,
-    "LinearResistance / static": LinearResistanceStatic,
-    "LevelBoundary / static": LevelBoundaryStatic,
-    "ManningResistance / static": ManningResistanceStatic,
-    "Pump / static": PumpStatic,
-    "Terminal / static": TerminalStatic,
-    "FlowBoundary / static": FlowBoundaryStatic,
+class ControlCondition(Input):
+    input_type = "Control / condition"
+    geometry_type = "No Geometry"
+    attributes = [
+        QgsField("node_id", QVariant.Int),
+        QgsField("listen_node_id", QVariant.Int),
+        QgsField("variable", QVariant.String),
+        QgsField("greater_than", QVariant.Double),
+    ]
+
+
+class ControlLogic(Input):
+    input_type = "Control / logic"
+    geometry_type = "LineString"
+    attributes = [
+        QgsField("node_id", QVariant.Int),
+        QgsField("control_state", QVariant.String),
+        QgsField("truth_state", QVariant.String),
+    ]
+
+
+NODES = {cls.input_type: cls for cls in Input.__subclasses__()}
+NONSPATIALNODETYPES = {
+    cls.nodetype() for cls in Input.__subclasses__() if not cls.is_spatial()
 }
 
 

docs/contribute/addnode.qmd
@@ -148,7 +148,6 @@ In `python/ribasim/ribasim/node.py` add a color and shape description in the `MA
 
 The script `qgis/core/nodes.py` has to be updated to specify how the new node type is displayed by the QGIS plugin. Specifically:
 
-- Add `"NewNodeType: NewNodeType",` to the `"map"` dictionary in `Node.set_editor_widget`;
 - Add a color and shape description in the `MARKERS` dictionary in `Node.renderer`,
   consistent with the entries added above;
 - Add an input class per schema, e.g.
@@ -162,8 +161,6 @@ class NewNodeTypeStatic:
         # Other fields for properties of this node
     ]
 ```
-
-and add these to the NODES dictionary.
 
 
 # Tests

commit ca41fb9f5aa6102bc8668bed96ca8e8684f08db4

draw edges without edge_type (#350)
I noticed there was one small regression in #335. When drawing new
edges, they were invisible, until you stopped editing. At that point the
plugin would fill in the current default edge_type = "flow" and it would
show up in blue.

With this PR the newly created edges are visible in black, until they
are turned into flow edges and become blue again.
The node markers already have a similar "other / unknown / new" marker
for this purpose, it makes sense to have that for edges as well.

![image](https://github.com/Deltares/Ribasim/assets/4471859/f39aa012-dba4-4cf0-8ca1-2be9e3a7a5d4)

qgis/core/nodes.py
@@ -231,6 +231,7 @@ class Edge(Input):
         MARKERS = {
             "flow": (QColor("#3690c0"), "flow"),  # lightblue
             "control": (QColor("gray"), "control"),
+            "": (QColor("black"), ""),  # All other edges, or incomplete input
         }
 
         categories = []