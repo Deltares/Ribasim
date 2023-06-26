c928a1c8ac0a5ccf83617fa6d162d348984a1ecf
Bart de Koning <bart123-4@hotmail.com>
2023-06-26 13:55:26 +0200

    test_fix

--- core/test/utils.jl
+++ core/test/utils.jl
@@ -22,6 +22,7 @@ end
     area = [[0.0, 1.0], [0.0, 1.0]]
     level = [[0.0, 1.0], [4.0, 5.0]]
     storage = Ribasim.profile_storage.(level, area)
+    target_level = [0.0, 0.0]
     basin = Ribasim.Basin(
         Indices([5, 7]),
         [2.0, 3.0],
@@ -32,6 +33,7 @@ end
         area,
         level,
         storage,
+        target_level,
         StructVector{Ribasim.BasinForcingV1}(undef, 0),
     )
 
