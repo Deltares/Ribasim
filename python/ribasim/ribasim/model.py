98eba085cf05ce176449e44fe308f99292a4c519
Bart de Koning <bart123-4@hotmail.com>
2023-07-12 14:14:22 +0200

    Fix examples notebook

--- python/ribasim/ribasim/model.py
+++ python/ribasim/ribasim/model.py
@@ -335,7 +335,7 @@ class Model(BaseModel):
                         y_end.append(np.mean(edge_line.xy[1]))
 
                     else:
-                        point_end = self.node.static.iloc[node_id - 1]
+                        point_end = self.node.static.iloc[node_id - 1].geometry
                         x_end.append(point_end.x)
                         y_end.append(point_end.y)
 
