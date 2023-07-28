c0349ba8e3f0bcc55d8f76d5c01b56018fe5ac8c
Bart de Koning <74617371+SouthEndMusic@users.noreply.github.com>
2023-07-28 08:48:45 +0200

    Jacobian prototype (sparsity) (#459)
    Fixes part of https://github.com/Deltares/Ribasim/issues/419.
    
    ---------
    
    Co-authored-by: Bart de Koning <bart123-4@hotmail.com>
    Co-authored-by: Martijn Visser <mgvisser@gmail.com>

--- core/src/bmi.jl
+++ core/src/bmi.jl
@@ -54,8 +54,12 @@ function BMI.initialize(T::Type{Model}, config::Config)::Model
     # for Float32 this method allows max ~1000 year simulations without accuracy issues
     @assert eps(t_end) < 3600 "Simulation time too long"
     timespan = (zero(t_end), t_end)
+
+    jac_prototype = get_jac_prototype(parameters)
+    RHS = ODEFunction(water_balance!; jac_prototype)
+
     @timeit_debug to "Setup ODEProblem" begin
-        prob = ODEProblem(water_balance!, u0, timespan, parameters)
+        prob = ODEProblem(RHS, u0, timespan, parameters)
     end
 
     callback, saved_flow = create_callbacks(parameters)
