399634cc0e60af86150f6ac236879ef38f095407
Bart de Koning <bart123-4@hotmail.com>
2023-08-08 17:04:55 +0200

    Add weir to Ribasim python

---
+++ python/ribasim/ribasim/node_types/weir.py
@@ -0,0 +1,32 @@
+import pandera as pa
+from pandera.engines.pandas_engine import PydanticModel
+from pandera.typing import DataFrame
+
+from ribasim import models
+from ribasim.input_base import TableModel
+
+__all__ = ("Weir",)
+
+
+class StaticSchema(pa.SchemaModel):
+    class Config:
+        """Config with dataframe-level data type."""
+
+        dtype = PydanticModel(models.WeirStatic)
+
+
+class Weir(TableModel):
+    """
+    Conducts water from a source node to a destination node.
+    The set flow rate will be used unless the intake storage is less than 10m3,
+    in which case the flow rate will be linearly reduced to 0 m3/s.
+    Negative flow rates are not supported.
+    Note that the intake must always be a Basin.
+
+    Parameters
+    ----------
+    static : pd.DataFrame
+        Table with constant flow rates.
+    """
+
+    static: DataFrame[StaticSchema]
