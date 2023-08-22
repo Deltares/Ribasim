Deltares/Ribasim

commit 37ec8d8418f834641a87418699fec48cc2a2d798

Update QGIS plugin

qgis/core/nodes.py
@@ -476,6 +476,21 @@ class PidControlStatic(Input):
         QgsField("node_id", QVariant.Int),
         QgsField("active", QVariant.Bool),
         QgsField("listen_node_id", QVariant.Int),
+        QgsField("target", QVariant.Float64),
+        QgsField("proportional", QVariant.Double),
+        QgsField("integral", QVariant.Double),
+        QgsField("derivative", QVariant.Double),
+    ]
+
+
+class PidControlTime(Input):
+    input_type = "PidControl / static"
+    geometry_type = "LineString"
+    attributes = [
+        QgsField("node_id", QVariant.Int),
+        QgsField("listen_node_id", QVariant.Int),
+        QgsField("time", QVariant.DateTime),
+        QgsField("target", QVariant.Float64),
         QgsField("proportional", QVariant.Double),
         QgsField("integral", QVariant.Double),
         QgsField("derivative", QVariant.Double),

commit 422389899deb620eefbb06ee21e0e32ace5cd8eb

attempt to fix QGIS tests (#525)
Failing on master since 37ec8d8418f834641a87418699fec48cc2a2d798. I
thought the failure in that PR was unrelated, but I hope it's not.

qgis/core/nodes.py
@@ -476,7 +476,7 @@ class PidControlStatic(Input):
         QgsField("node_id", QVariant.Int),
         QgsField("active", QVariant.Bool),
         QgsField("listen_node_id", QVariant.Int),
-        QgsField("target", QVariant.Float64),
+        QgsField("target", QVariant.Double),
         QgsField("proportional", QVariant.Double),
         QgsField("integral", QVariant.Double),
         QgsField("derivative", QVariant.Double),
@@ -490,7 +490,7 @@ class PidControlTime(Input):
         QgsField("node_id", QVariant.Int),
         QgsField("listen_node_id", QVariant.Int),
         QgsField("time", QVariant.DateTime),
-        QgsField("target", QVariant.Float64),
+        QgsField("target", QVariant.Double),
         QgsField("proportional", QVariant.Double),
         QgsField("integral", QVariant.Double),
         QgsField("derivative", QVariant.Double),