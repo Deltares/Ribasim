fe60eaa513e9fd44bec30b3596b9fbcc4dfec603
Hofer-Julian <julianhofer@gnome.org>
2023-08-29 16:43:07 +0200

    Continue

--- python/ribasim_testmodels/tests/test_models.py
+++ python/ribasim_testmodels/tests/test_models.py
@@ -1,3 +1,6 @@
+import subprocess
+from pathlib import Path
+
 import pytest
 import ribasim
 import ribasim_testmodels
@@ -7,6 +10,18 @@ import ribasim_testmodels
     "model_constructor",
     map(ribasim_testmodels.__dict__.get, ribasim_testmodels.__all__),
 )
-def test_models(model_constructor):
+def test_models(model_constructor, tmp_path):
     model = model_constructor()
     assert isinstance(model, ribasim.Model)
+    model.write(tmp_path)
+
+    executable = (
+        Path(__file__).parents[3]
+        / "build"
+        / "create_binaries"
+        / "ribasim_cli"
+        / "bin"
+        / "ribasim.exe"
+    )
+    config_file = str(tmp_path / f"{model.modelname}.toml")
+    subprocess.run([executable, config_file], check=True)
