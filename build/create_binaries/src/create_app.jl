8366c28d335d8ec1ed8bae2712aa104425bab420
Martijn Visser <mgvisser@gmail.com>
2023-11-10 11:11:30 +0100

    move build code into create_binaries package (#762)
    Follow up to #740.
    
    ---------
    
    Co-authored-by: Hofer-Julian <30049909+Hofer-Julian@users.noreply.github.com>

---
+++ build/create_binaries/src/create_app.jl
@@ -0,0 +1,32 @@
+"Build the Ribasim CLI using PackageCompiler"
+function build_app()
+    project_dir = "../ribasim_cli"
+    license_file = "../../LICENSE"
+    output_dir = "ribasim_cli"
+    git_repo = "../.."
+
+    create_app(
+        project_dir,
+        output_dir;
+        # map from binary name to julia function name
+        executables = ["ribasim" => "julia_main"],
+        precompile_execution_file = "precompile.jl",
+        include_lazy_artifacts = true,
+        force = true,
+    )
+
+    add_metadata(project_dir, license_file, output_dir, git_repo)
+
+    # On Windows, write ribasim.cmd in the output_dir, that starts ribasim.exe.
+    # Since the bin dir contains a julia.exe and many DLLs that you may not want in your path,
+    # with this script you can put output_dir in your path instead.
+    if Sys.iswindows()
+        cmd = raw"""
+        @echo off
+        "%~dp0bin\ribasim.exe" %*
+        """
+        open(normpath(output_dir, "ribasim.cmd"); write = true) do io
+            print(io, cmd)
+        end
+    end
+end
