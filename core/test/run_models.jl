50c8675d13fd1c437b015b35d65b63de589003f8
Maarten Pronk <8655030+evetion@users.noreply.github.com>
2023-11-17 17:46:48 +0100

    Support arrow input. (#798)
    Fixes #170

--- core/test/run_models.jl
+++ core/test/run_models.jl
@@ -66,6 +66,14 @@ end
     end
 end
 
+@testset "basic arrow model" begin
+    toml_path = normpath(@__DIR__, "../../generated_testmodels/basic_arrow/ribasim.toml")
+    @test ispath(toml_path)
+    model = Ribasim.run(toml_path)
+    @test model isa Ribasim.Model
+    @test successful_retcode(model)
+end
+
 @testset "basic transient model" begin
     toml_path =
         normpath(@__DIR__, "../../generated_testmodels/basic_transient/ribasim.toml")
