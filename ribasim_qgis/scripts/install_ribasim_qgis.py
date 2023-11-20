3b6e3ff0a36da4888e4cbfdd50beffdefc8f3e30
Marnix Kraus <marnix.kraus@deltares.nl>
2023-11-20 17:16:06 +0100

    Use requests to download imod latest release

--- ribasim_qgis/scripts/install_ribasim_qgis.py
+++ ribasim_qgis/scripts/install_ribasim_qgis.py
@@ -1,7 +1,11 @@
 from pathlib import Path
 
+from enable_plugin import enable_plugin
+
 target_path = Path("ribasim_qgis").absolute()
 source_path = Path(".pixi/env/Library/python/plugins/ribasim_qgis")
 
 source_path.unlink(missing_ok=True)
 source_path.symlink_to(target_path, target_is_directory=True)
+
+enable_plugin("ribasim_qgis")
