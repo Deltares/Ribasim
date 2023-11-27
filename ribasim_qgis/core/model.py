d9af54dff4e2bd3b4394ea92491d9498e3969959
Marnix Kraus <marnix.kraus@deltares.nl>
2023-11-27 13:25:44 +0100

    Simplify retrieval of database.gpkg from toml file

--- ribasim_qgis/core/model.py
+++ ribasim_qgis/core/model.py
@@ -18,3 +18,18 @@ def get_directory_path_from_model_file(model_path: Path, *, property: str) -> Pa
     # The .joinpath method (/) of pathlib.Path will take care of an absolute input_dir.
     # No need to check it ourselves!
     return (Path(model_path).parent / found_property).resolve()
+
+
+def get_database_path_from_model_file(model_path: Path) -> Path:
+    """Get the database path database.gpkg based on the model file's input_dir.
+
+    Args:
+        model_path (Path): Path to the model (.toml) file.
+
+    Returns_:
+        Path: The full path to database.gpkg
+    """
+    return (
+        get_directory_path_from_model_file(model_path, property="input_dir")
+        / "database.gpkg"
+    )
