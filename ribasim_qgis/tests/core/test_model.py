e7d8e7eae80b103a7dcfe8596f21d5abb978029b
Marnix Kraus <marnix.kraus@deltares.nl>
2023-12-04 10:11:51 +0100

    Simplify doc comments

--- ribasim_qgis/tests/core/test_model.py
+++ ribasim_qgis/tests/core/test_model.py
@@ -13,9 +13,7 @@ class TestModel(unittest.TestCase):
     data_folder_path = tests_folder_path / "data"
 
     def test_get_directory_path_from_model_file(self):
-        """_summary
-        Tests that get_directory_path_from_model_file() can resolve paths from a toml file.
-        """
+        """Tests that get_directory_path_from_model_file() can resolve paths from a toml file."""
         for test_case in [("input_dir", "."), ("results_dir", "results")]:
             with self.subTest(property=test_case[0], value=test_case[1]):
                 path = get_directory_path_from_model_file(
@@ -30,9 +28,7 @@ class TestModel(unittest.TestCase):
                 self.assertEqual(path, self.data_folder_path / test_case[1])
 
     def test_get_database_path_from_model_file(self):
-        """_summary
-        Tests that get_database_path_from_model_file() can find the input directory and appends the database.gpkg to it.
-        """
+        """Tests that get_database_path_from_model_file() can find the input directory and appends the database.gpkg to it."""
         path = get_database_path_from_model_file(
             self.data_folder_path / "simple_valid.toml"
         )
