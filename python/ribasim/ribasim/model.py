725bca5483e3e5f10e7ae0b9e9b29627b933e96c
Martijn Visser <mgvisser@gmail.com>
2023-12-07 13:08:01 +0100

    Clarify `validate_model_node_ids` (#867)
    Fixes #865.
    
    Net output:
    
    ```
    For LevelBoundary, the node IDs in the data tables don't match the node IDs in the network.
        Node IDs only in the data tables: {181, 182, 183, 184, 185, 186, 187, 188, 189}.
        Node IDs only in the network: set().
    ```

--- python/ribasim/ribasim/model.py
+++ python/ribasim/ribasim/model.py
@@ -305,19 +305,28 @@ class Model(FileModel):
             raise ValueError(" ".join(msg))
 
     def validate_model_node_ids(self):
-        """Check whether the node IDs in the node field correspond to the node IDs on the node type fields."""
+        """Check whether the node IDs in the data tables correspond to the node IDs in the network."""
 
         error_messages = []
 
         for node in self.nodes().values():
-            node_ids_field = node.node_ids()
-            node_ids_from_node_field = self.network.node.df.loc[
-                self.network.node.df["type"] == node.get_input_type()
-            ].index
+            nodetype = node.get_input_type()
+            if nodetype == "Network":
+                # skip the reference
+                continue
+            node_ids_data = set(node.node_ids())
+            node_ids_network = set(
+                self.network.node.df.loc[self.network.node.df["type"] == nodetype].index
+            )
 
-            if not set(node_ids_from_node_field) == set(node_ids_field):
+            if not node_ids_network == node_ids_data:
+                extra_in_network = node_ids_network.difference(node_ids_data)
+                extra_in_data = node_ids_data.difference(node_ids_network)
                 error_messages.append(
-                    f"The node IDs in the field {node} {node_ids_field} do not correspond with the node IDs in the field node {node_ids_from_node_field.tolist()}."
+                    f"""For {nodetype}, the node IDs in the data tables don't match the node IDs in the network.
+    Node IDs only in the data tables: {extra_in_data}.
+    Node IDs only in the network: {extra_in_network}.
+                    """
                 )
 
         if len(error_messages) > 0:
