5e85f1ab328c1e33dd51ac8838a0e6219568bdbf
Marnix Kraus <marnix.kraus@deltares.nl>
2023-12-14 15:42:42 +0100

    Move main code to Ribasim.jl

--- core/src/Ribasim.jl
+++ core/src/Ribasim.jl
@@ -60,6 +60,7 @@ using OrdinaryDiffEq
 using OrdinaryDiffEq: OrdinaryDiffEqRosenbrockAdaptiveAlgorithm
 using PreallocationTools: DiffCache, FixedSizeDiffCache, get_tmp
 using SciMLBase
+using SciMLBase: successful_retcode
 using SparseArrays
 using SQLite: SQLite, DB, Query, esc_id
 using StructArrays: StructVector
@@ -81,5 +82,49 @@ include("io.jl")
 include("create.jl")
 include("bmi.jl")
 include("consts.jl")
+
+function help(x)::Cint
+    println(x)
+    println("Usage: ribasim path/to/model/ribasim.toml")
+    return 1
+end
+
+function main(ARGS)::Cint
+    n = length(ARGS)
+    if n != 1
+        return help("Exactly 1 argument expected, got $n")
+    end
+    arg = only(ARGS)
+
+    if arg == "--version"
+        version = pkgversion(Ribasim)
+        print(version)
+        return 0
+    end
+
+    if !isfile(arg)
+        return help("File not found: $arg")
+    end
+
+    try
+        # show progress bar in terminal
+        model = with_logger(TerminalLogger()) do
+            Ribasim.run(arg)
+        end
+        return if successful_retcode(model)
+            println("The model finished successfully")
+            0
+        else
+            t = Ribasim.datetime_since(model.integrator.t, model.config.starttime)
+            retcode = model.integrator.sol.retcode
+            println("The model exited at model time $t with return code $retcode")
+            println("See https://docs.sciml.ai/DiffEqDocs/stable/basics/solution/#retcodes")
+            1
+        end
+    catch
+        Base.invokelatest(Base.display_error, current_exceptions())
+        return 1
+    end
+end
 
 end  # module Ribasim
