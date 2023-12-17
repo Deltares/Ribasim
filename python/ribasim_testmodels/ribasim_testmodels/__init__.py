3ad0da0da6575a8123a490a759d6b9b5baf67ee3
Bart de Koning <74617371+SouthEndMusic@users.noreply.github.com>
2023-12-17 20:58:35 +0100

    Allocation fractional flow constraints (#854)
    Fixes https://github.com/Deltares/Ribasim/issues/727.

--- python/ribasim_testmodels/ribasim_testmodels/__init__.py
+++ python/ribasim_testmodels/ribasim_testmodels/__init__.py
@@ -6,8 +6,9 @@ import ribasim
 
 import ribasim_testmodels
 from ribasim_testmodels.allocation import (
-    minimal_subnetwork_model,
     # looped_subnetwork_model,
+    fractional_flow_subnetwork_model,
+    minimal_subnetwork_model,
     subnetwork_model,
     user_model,
 )
@@ -78,6 +79,7 @@ __all__ = [
     "user_model",
     "subnetwork_model",
     "minimal_subnetwork_model",
+    "fractional_flow_subnetwork_model",
     # Disable until this issue is resolved:
     # https://github.com/Deltares/Ribasim/issues/692
     # "looped_subnetwork_model",
