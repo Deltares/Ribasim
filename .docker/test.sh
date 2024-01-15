0d605c921f3f9c6545b2e003469c4a9b1f28a55d
Maarten Pronk <git@evetion.nl>
2024-01-15 14:56:48 +0100

    No sleep on end.

--- .docker/test.sh
+++ .docker/test.sh
@@ -3,4 +3,4 @@ set -euxo pipefail
 
 source .env
 
-docker exec -t qgis sh -c "cd /tests_directory && xvfb-run -a qgis_testrunner.sh ${PLUGIN_NAME}.tests && sleep 5"
+docker exec -t qgis sh -c "cd /tests_directory && xvfb-run -a qgis_testrunner.sh ${PLUGIN_NAME}.tests"
