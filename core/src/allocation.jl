ef555f3f0e12a8fa2c32d4cf680bcf06ac377e6a
Bart de Koning <bart.dekoning@deltares.nl>
2024-01-15 16:18:33 +0100

    Add absolute value constraints for subnetworks treated as users

--- core/src/allocation.jl
+++ core/src/allocation.jl
@@ -568,11 +568,21 @@ function add_constraints_absolute_value!(
     config::Config,
 )::Nothing
     (; graph) = p
-    node_ids = graph[].node_ids[allocation_network_id]
 
     objective_type = config.allocation.objective_type
     if startswith(objective_type, "linear")
+        node_ids = graph[].node_ids[allocation_network_id]
         node_ids_user = [node_id for node_id in node_ids if graph[node_id].type == :user]
+
+        # For the main network, connections to subnetworks are treated as users
+        if allocation_network_id == 1
+            for connections_subnetwork in main_network_connections
+                for connection in connections_subnetwork
+                    push!(node_ids_user, connection[2])
+                end
+            end
+        end
+
         node_ids_user_inflow = Dict(
             node_id_user => only(inflow_ids_allocation(graph, node_id_user)) for
             node_id_user in node_ids_user
