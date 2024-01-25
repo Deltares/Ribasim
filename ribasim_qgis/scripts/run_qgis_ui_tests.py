81bd9eea7d2a17e1fb372bc04ddc9bbec3a01880
Marnix Kraus <marnix.kraus@deltares.nl>
2024-01-25 14:08:08 +0100

    Run qgis simple unit tests automated on github
    Also post code coverage

--- ribasim_qgis/scripts/run_qgis_ui_tests.py
+++ ribasim_qgis/scripts/run_qgis_ui_tests.py
@@ -18,5 +18,7 @@ qgis_process = subprocess.run(
 
 print(qgis_process.stdout)
 qgis_process.check_returncode()
+
+# QGIS always finishes with exit code 0, even when tests fail, so we have to check the output
 if any(s in qgis_process.stdout for s in ["QGIS died on signal", "FAILED"]):
     exit(1)
