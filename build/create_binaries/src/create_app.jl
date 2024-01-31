92eddaadc40dd66915b200278378286120eed273
Martijn Visser <mgvisser@gmail.com>
2024-01-31 22:25:46 +0100

    Fold ribasim_cli into root project
    Easy to do since ribasim_cli is a thin wrapper over Ribasim.main nowadays.

--- build/create_binaries/src/create_app.jl
+++ build/create_binaries/src/create_app.jl
@@ -1,6 +1,30 @@
-"Build the Ribasim CLI using PackageCompiler"
+"""
+    build_app()
+
+Build the Ribasim CLI using PackageCompiler.
+
+This builds the `main` function from the Ribasim Julia package into a command line interface
+(cli) application using PackageCompiler.jl.
+
+This enables using Ribasim without having to install Julia, and thus makes it more
+convenient to use in certain settings where installation must be simple and no interactive
+Julia session is needed.
+
+If you have installed Julia and Ribasim, a simulation can also be started from the command
+line as follows:
+
+```
+julia --eval 'using Ribasim; Ribasim.main("path/to/model/ribasim.toml")'
+```
+
+With a Ribasim CLI build this becomes:
+
+```
+ribasim path/to/model/ribasim.toml
+```
+"""
 function build_app()
-    project_dir = "../ribasim_cli"
+    project_dir = "../../core"
     license_file = "../../LICENSE"
     output_dir = "ribasim_cli"
     git_repo = "../.."
@@ -9,7 +33,7 @@ function build_app()
         project_dir,
         output_dir;
         # map from binary name to julia function name
-        executables = ["ribasim" => "julia_main"],
+        executables = ["ribasim" => "main"],
         precompile_execution_file = "precompile.jl",
         include_lazy_artifacts = true,
         force = true,
