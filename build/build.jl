4c4bb52415db8f7e1683a9ec97783c6e3f8b6268
Martijn Visser <mgvisser@gmail.com>
2024-02-01 15:57:26 +0100

    Keep only the root and core project (#1021)
    This attempts to simplifiy our Julia project and build setup.
    
    Where possible, existing projects are moved into the root project like
    in #1014. All core dependencies are now also direct root dependencies to
    make using the root project more convenient for development.
    
    While working on this I realized that we can remove the `libribasim` and
    `ribasim_cli` projects entirely. `libribasim` is now a module of the
    Ribasim package, and `ribasim_cli` already was just a tiny wrapper
    around `Ribasim.main`. The content of the README.md files from those two
    folders now reside in the `build_lib` and `build_app` docstring.
    
    Since that left only the `create_binaries` folder under `/build`, I
    collapsed that level out. `create_binaries` is no longer a package, but
    the code is still used, but now simply directly included in `build.jl`.

--- build/build.jl
+++ build/build.jl
@@ -1,4 +1,11 @@
-using create_binaries
+using Artifacts
+using PackageCompiler
+using TOML
+using LibGit2
+
+include("src/add_metadata.jl")
+include("src/create_app.jl")
+include("src/create_lib.jl")
 
 """
 Build the Ribasim CLI, libribasim, or both, using PackageCompiler.
