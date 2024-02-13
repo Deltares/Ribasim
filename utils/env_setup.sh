80b7d0845918d0bb2e293ff555b90b119417b7be
deltamarnix <150045289+deltamarnix@users.noreply.github.com>
2024-02-13 13:56:03 +0100

    Set mypypath dynamically (#1116)
    Dynamically try to get the relative path for the current working
    environment to set `MYPYPATH`

--- utils/env_setup.sh
+++ utils/env_setup.sh
@@ -3,3 +3,6 @@ JULIAUP_DEPOT_PATH=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null &
 export JULIAUP_DEPOT_PATH
 QUARTO_PYTHON=python
 export QUARTO_PYTHON
+relative_conda_prefix=$(realpath --relative-to="$PWD" "$CONDA_PREFIX")
+MYPYPATH=$relative_conda_prefix/share/qgis/python
+export MYPYPATH
