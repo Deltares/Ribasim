cbaa6f3d0d0a44543b2a69d2a39481bcde8e554c
Hofer-Julian <julianhofer@gnome.org>
2024-03-25 12:54:53 +0100

    Don't use all cores

--- utils/generate-testmodels.py
+++ utils/generate-testmodels.py
@@ -1,4 +1,5 @@
 import multiprocessing
+import os
 import shutil
 from functools import partial
 from pathlib import Path
@@ -32,7 +33,12 @@ Don't put important stuff in here, it will be emptied for every run."""
     generate_model_partial = partial(generate_model, datadir=datadir)
 
     testmodels = list(ribasim_testmodels.constructors.items())
-    with alive_bar(len(testmodels)) as bar, multiprocessing.Pool() as p:
+    cpu_count = os.cpu_count()
+    number_processes = 4 if cpu_count is None else int(cpu_count / 2)
+    with (
+        alive_bar(len(testmodels)) as bar,
+        multiprocessing.Pool(number_processes) as p,
+    ):
         for model_name in p.imap_unordered(generate_model_partial, testmodels):
             print(f"Generated {model_name}")
             bar()
