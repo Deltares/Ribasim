0cc5790fd0e17b9532f59f05f14b3a443eb8693a
Hofer-Julian <julianhofer@gnome.org>
2024-03-25 11:22:06 +0100

    Parallelize generate testmodels

--- utils/generate-testmodels.py
+++ utils/generate-testmodels.py
@@ -1,7 +1,18 @@
+import multiprocessing
 import shutil
+from functools import partial
 from pathlib import Path
 
 import ribasim_testmodels
+from alive_progress import alive_bar
+
+
+def generate_model(args, datadir):
+    model_name, model_constructor = args
+    model = model_constructor()
+    model.write(datadir / model_name / "ribasim.toml")
+    return model_name
+
 
 if __name__ == "__main__":
     datadir = Path("generated_testmodels")
@@ -18,7 +29,10 @@ The content of this directory are generated testmodels for Ribasim
 Don't put important stuff in here, it will be emptied for every run."""
     )
 
-    for model_name, model_constructor in ribasim_testmodels.constructors.items():
-        print(f"Generating {model_name}")
-        model = model_constructor()
-        model.write(datadir / model_name / "ribasim.toml")
+    generate_model_partial = partial(generate_model, datadir=datadir)
+
+    testmodels = list(ribasim_testmodels.constructors.items())
+    with alive_bar(len(testmodels)) as bar, multiprocessing.Pool() as p:
+        for model_name in p.imap_unordered(generate_model_partial, testmodels):
+            print(f"Generated {model_name}")
+            bar()
