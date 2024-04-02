d1df84d6182128f1775f2102e4dc5dbfbb8f8344
Bart de Koning <bart.dekoning@deltares.nl>
2024-04-02 13:14:26 +0200

    comments adressed

--- core/src/solve.jl
+++ core/src/solve.jl
@@ -293,9 +293,11 @@ function formulate_flow!(
         # effectively allocated = demand.
         for priority_idx in eachindex(allocated[i])
             alloc_prio = allocated[i][priority_idx]
-            demand_prio =
-                demand_from_timeseries[i] ? demand_itp[i][priority_idx](t) :
+            demand_prio = if demand_from_timeseries[i]
+                demand_itp[i][priority_idx](t)
+            else
                 get_user_demand(p, id, priority_idx; reduced = false)
+            end
             alloc = min(alloc_prio, demand_prio)
             q += alloc
         end
