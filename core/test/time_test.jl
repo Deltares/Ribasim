f42250ab0ccca2908bbac292525d28eea5733266
Martijn Visser <mgvisser@gmail.com>
2024-04-08 12:44:39 +0200

    Don't add the endtime to basin.arrow results (#1359)
    This reverts a part of 4ff9987710fac6642d723d97bceb574517b09f24
    
    On Tuesday we discussed the `basin.arrow` with @SouthEndMusic and
    @Huite. A few issues/PRs will come out of this, but this already removes
    the endtime, such that we don't have `missing` vertical fluxes in
    `basin.arrow` anymore. The downside is that the final storage is not
    currently available, but we have #1358 for that, and plan to add `dS/dt
    (m3/s)` as well to be able to compute it.

--- core/test/time_test.jl
+++ core/test/time_test.jl
@@ -34,15 +34,10 @@ end
     n_basin = length(basin.node_id)
     basin_table = DataFrame(Ribasim.basin_table(model))
 
-    # No vertical flux data for last saveat
-    t_end = last(basin_table).time
-    data_end = filter(:time => t -> t == t_end, basin_table)
-    @test all(ismissing.(data_end.precipitation))
-    @test all(ismissing.(data_end.evaporation))
-    @test all(ismissing.(data_end.drainage))
-    @test all(ismissing.(data_end.infiltration))
-
     time_table = DataFrame(basin.time)
+    t_end = time_table.time[end]
+    filter!(:time => t -> t !== t_end, time_table)
+
     time_table[!, "basin_idx"] = [
         Ribasim.id_index(basin.node_id, node_id)[2] for
         node_id in Ribasim.NodeID.(:Basin, time_table.node_id)
@@ -65,9 +60,6 @@ end
             time_table.mean_area[idx_1] = mean_area
         end
     end
-
-    filter!(:time => t -> t !== t_end, basin_table)
-    filter!(:time => t -> t !== t_end, time_table)
 
     @test all(
         isapprox(
