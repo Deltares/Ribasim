Deltares/Ribasim

commit 169466e658f526f2316c05698c7fac0765e43a33

Discrete control with discrete callback (#1393)
Fixes https://github.com/Deltares/Ribasim/issues/455, fixes
https://github.com/Deltares/Ribasim/issues/495. The latter is made
redundant by this PR since U/D truth values are removed.

python/ribasim_testmodels/ribasim_testmodels/__init__.py
@@ -31,7 +31,7 @@ from ribasim_testmodels.discrete_control import (
     compound_variable_condition_model,
     flow_condition_model,
     level_boundary_condition_model,
-    level_setpoint_with_minmax_model,
+    level_range_model,
     pump_discrete_control_model,
     tabulated_rating_curve_control_model,
 )
@@ -79,7 +79,7 @@ __all__ = [
     "leaky_bucket_model",
     "level_boundary_condition_model",
     "level_demand_model",
-    "level_setpoint_with_minmax_model",
+    "level_range_model",
     "linear_resistance_demand_model",
     "linear_resistance_model",
     "local_pidcontrolled_cascade_model",

core/src/callback.jl
@@ -1,35 +1,3 @@
-"""
-Set parameters of nodes that are controlled by DiscreteControl to the
-values corresponding to the initial state of the model.
-"""
-function set_initial_discrete_controlled_parameters!(
-    integrator,
-    storage0::Vector{Float64},
-)::Nothing
-    (; p) = integrator
-    (; discrete_control) = p
-
-    n_conditions = sum(length(vec) for vec in discrete_control.condition_value; init = 0)
-    condition_diffs = zeros(Float64, n_conditions)
-    discrete_control_condition(condition_diffs, storage0, integrator.t, integrator)
-
-    # Set the discrete control value (bool) per compound variable
-    idx_start = 1
-    for (compound_variable_idx, vec) in enumerate(discrete_control.condition_value)
-        l = length(vec)
-        idx_end = idx_start + l - 1
-        discrete_control.condition_value[compound_variable_idx] .=
-            (condition_diffs[idx_start:idx_end] .> 0)
-        idx_start += l
-    end
-
-    # For every discrete_control node find a condition_idx it listens to
-    for discrete_control_node_id in unique(discrete_control.node_id)
-        condition_idx =
-            searchsortedfirst(discrete_control.node_id, discrete_control_node_id)
-        discrete_control_affect!(integrator, condition_idx, missing)
-    end
-end
 
 """
 Create the different callbacks that are used to store results
@@ -89,13 +57,7 @@ function create_callbacks(
 
     n_conditions = sum(length(vec) for vec in discrete_control.greater_than; init = 0)
     if n_conditions > 0
-        discrete_control_cb = VectorContinuousCallback(
-            discrete_control_condition,
-            discrete_control_affect_upcrossing!,
-            discrete_control_affect_downcrossing!,
-            n_conditions;
-            save_positions = (false, false),
-        )
+        discrete_control_cb = FunctionCallingCallback(apply_discrete_control!)
         push!(callbacks, discrete_control_cb)
     end
     callback = CallbackSet(callbacks...)
@@ -186,33 +148,50 @@ function save_vertical_flux(u, t, integrator)
     return vertical_flux_mean
 end
 
-"""
-Listens for changes in condition truths.
-"""
-function discrete_control_condition(out, u, t, integrator)
+function apply_discrete_control!(u, t, integrator)::Nothing
     (; p) = integrator
     (; discrete_control) = p
     condition_idx = 0
 
+    discrete_control_condition!(u, t, integrator)
+
+    # For every compound variable see whether it changes a control state
+    for compound_variable_idx in eachindex(discrete_control.node_id)
+        discrete_control_affect!(integrator, compound_variable_idx)
+    end
+end
+
+"""
+Update discrete control condition truths.
+"""
+function discrete_control_condition!(u, t, integrator)
+    (; p) = integrator
+    (; discrete_control) = p
+
     # Loop over compound variables
-    for (listen_node_ids, variables, weights, greater_thans, look_aheads) in zip(
+    for (
+        listen_node_ids,
+        variables,
+        weights,
+        greater_thans,
+        look_aheads,
+        condition_values,
+    ) in zip(
         discrete_control.listen_node_id,
         discrete_control.variable,
         discrete_control.weight,
         discrete_control.greater_than,
         discrete_control.look_ahead,
+        discrete_control.condition_value,
     )
         value = 0.0
         for (listen_node_id, variable, weight, look_ahead) in
             zip(listen_node_ids, variables, weights, look_aheads)
             value += weight * get_value(p, listen_node_id, variable, look_ahead, u, t)
         end
-        # Loop over greater_than values for this compound_variable
-        for greater_than in greater_thans
-            condition_idx += 1
-            diff = value - greater_than
-            out[condition_idx] = diff
-        end
+
+        condition_values .= false
+        condition_values[1:searchsortedlast(greater_thans, value)] .= true
     end
 end
 
@@ -263,104 +242,13 @@ function get_value(
 end
 
 """
-An upcrossing means that a condition (always greater than) becomes true.
-"""
-function discrete_control_affect_upcrossing!(integrator, condition_idx)
-    (; p, u, t) = integrator
-    (; discrete_control, basin) = p
-    (; variable, condition_value, listen_node_id) = discrete_control
-
-    compound_variable_idx, greater_than_idx =
-        get_discrete_control_indices(discrete_control, condition_idx)
-    condition_value[compound_variable_idx][greater_than_idx] = true
-
-    control_state_change = discrete_control_affect!(integrator, condition_idx, true)
-
-    # Check whether the control state change changed the direction of the crossing
-    # NOTE: This works for level conditions, but not for flow conditions on an
-    # arbitrary edge. That is because parameter changes do not change the instantaneous level,
-    # only possibly the du. Parameter changes can change the flow on an edge discontinuously,
-    # giving the possibility of logical paradoxes where certain parameter changes immediately
-    # undo the truth state that caused that parameter change.
-    listen_node_ids = discrete_control.listen_node_id[compound_variable_idx]
-    is_basin =
-        length(listen_node_ids) == 1 ? id_index(basin.node_id, only(listen_node_ids))[1] :
-        false
-
-    # NOTE: The above no longer works when listen feature ids can be something other than node ids
-    # I think the more durable option is to give all possible condition types a different variable string,
-    # e.g. basin.level and level_boundary.level
-    if variable[compound_variable_idx][1] == "level" && control_state_change && is_basin
-        # Calling water_balance is expensive, but it is a sure way of getting
-        # du for the basin of this level condition
-        du = zero(u)
-        water_balance!(du, u, p, t)
-        _, condition_basin_idx =
-            id_index(basin.node_id, listen_node_id[compound_variable_idx][1])
-
-        if du[condition_basin_idx] < 0.0
-            condition_value[compound_variable_idx][greater_than_idx] = false
-            discrete_control_affect!(integrator, condition_idx, false)
-        end
-    end
-end
-
-"""
-An downcrossing means that a condition (always greater than) becomes false.
-"""
-function discrete_control_affect_downcrossing!(integrator, condition_idx)
-    (; p, u, t) = integrator
-    (; discrete_control, basin) = p
-    (; variable, condition_value, listen_node_id) = discrete_control
-
-    compound_variable_idx, greater_than_idx =
-        get_discrete_control_indices(discrete_control, condition_idx)
-    condition_value[compound_variable_idx][greater_than_idx] = false
-
-    control_state_change = discrete_control_affect!(integrator, condition_idx, false)
-
-    # Check whether the control state change changed the direction of the crossing
-    # NOTE: This works for level conditions, but not for flow conditions on an
-    # arbitrary edge. That is because parameter changes do not change the instantaneous level,
-    # only possibly the du. Parameter changes can change the flow on an edge discontinuously,
-    # giving the possibility of logical paradoxes where certain parameter changes immediately
-    # undo the truth state that caused that parameter change.
-    compound_variable_idx, greater_than_idx =
-        get_discrete_control_indices(discrete_control, condition_idx)
-    listen_node_ids = discrete_control.listen_node_id[compound_variable_idx]
-    is_basin =
-        length(listen_node_ids) == 1 ? id_index(basin.node_id, only(listen_node_ids))[1] :
-        false
-
-    if variable[compound_variable_idx][1] == "level" && control_state_change && is_basin
-        # Calling water_balance is expensive, but it is a sure way of getting
-        # du for the basin of this level condition
-        du = zero(u)
-        water_balance!(du, u, p, t)
-        has_index, condition_basin_idx =
-            id_index(basin.node_id, listen_node_id[compound_variable_idx][1])
-
-        if has_index && du[condition_basin_idx] > 0.0
-            condition_value[compound_variable_idx][greater_than_idx] = true
-            discrete_control_affect!(integrator, condition_idx, true)
-        end
-    end
-end
-
-"""
 Change parameters based on the control logic.
 """
-function discrete_control_affect!(
-    integrator,
-    condition_idx::Int,
-    upcrossing::Union{Bool, Missing},
-)::Bool
+function discrete_control_affect!(integrator, compound_variable_idx)
     p = integrator.p
     (; discrete_control, graph) = p
 
-    # Get the discrete_control node that listens to this condition
-
-    compound_variable_idx, _ = get_discrete_control_indices(discrete_control, condition_idx)
+    # Get the discrete_control node to which this compound variable belongs
     discrete_control_node_id = discrete_control.node_id[compound_variable_idx]
 
     # Get the indices of all conditions that this control node listens to
@@ -376,56 +264,24 @@ function discrete_control_affect!(
     )
     truth_state = join(truth_values, "")
 
-    # Get the truth specific about the latest crossing
-    if !ismissing(upcrossing)
-        truth_value_idx =
-            condition_idx - sum(
-                length(vec) for
-                vec in discrete_control.condition_value[1:(where_node_id.start - 1)];
-                init = 0,
-            )
-        truth_values[truth_value_idx] = upcrossing ? "U" : "D"
-    end
-    truth_state_crossing_specific = join(truth_values, "")
-
     # What the local control state should be
     control_state_new =
-        if haskey(
-            discrete_control.logic_mapping,
-            (discrete_control_node_id, truth_state_crossing_specific),
-        )
-            truth_state_used = truth_state_crossing_specific
-            discrete_control.logic_mapping[(
-                discrete_control_node_id,
-                truth_state_crossing_specific,
-            )]
-        elseif haskey(
-            discrete_control.logic_mapping,
-            (discrete_control_node_id, truth_state),
-        )
-            truth_state_used = truth_state
+        if haskey(discrete_control.logic_mapping, (discrete_control_node_id, truth_state))
             discrete_control.logic_mapping[(discrete_control_node_id, truth_state)]
         else
             error(
-                "Control state specified for neither $truth_state_crossing_specific nor $truth_state for $discrete_control_node_id.",
+                "No control state specified for $discrete_control_node_id for truth state $truth_state.",
             )
         end
 
-    # What the local control state is
-    # TODO: Check time elapsed since control change
     control_state_now, _ = discrete_control.control_state[discrete_control_node_id]
-
-    control_state_change = false
-
     if control_state_now != control_state_new
-        control_state_change = true
-
         # Store control action in record
         record = discrete_control.record
 
         push!(record.time, integrator.t)
         push!(record.control_node_id, Int32(discrete_control_node_id))
-        push!(record.truth_state, truth_state_used)
+        push!(record.truth_state, truth_state)
         push!(record.control_state, control_state_new)
 
         # Loop over nodes which are under control of this control node
@@ -437,7 +293,7 @@ function discrete_control_affect!(
         discrete_control.control_state[discrete_control_node_id] =
             (control_state_new, integrator.t)
     end
-    return control_state_change
+    return nothing
 end
 
 function get_allocation_model(p::Parameters, allocation_network_id::Int32)::AllocationModel

core/src/model.jl
@@ -162,8 +162,6 @@ function Model(config::Config)::Model
         @show Ribasim.to
     end
 
-    set_initial_discrete_controlled_parameters!(integrator, storage)
-
     return Model(integrator, config, saved)
 end
 

core/src/util.jl
@@ -421,7 +421,7 @@ function expand_logic_mapping(
     logic_mapping_expanded = Dict{Tuple{NodeID, String}, String}()
 
     for (node_id, truth_state) in keys(logic_mapping)
-        pattern = r"^[TFUD\*]+$"
+        pattern = r"^[TF\*]+$"
         if !occursin(pattern, truth_state)
             error("Truth state \'$truth_state\' contains illegal characters or is empty.")
         end

core/src/validation.jl
@@ -563,7 +563,6 @@ function valid_discrete_control(p::Parameters, config::Config)::Bool
 
             if !isempty(undefined_control_states)
                 undefined_list = collect(undefined_control_states)
-                node_type = typeof(node).name.name
                 @error "These control states from $id are not defined for controlled $id_outneighbor: $undefined_list."
                 errors = true
             end

core/test/control_test.jl
@@ -145,39 +145,9 @@ end
     @test discrete_control.record.control_state == ["high", "low"]
     @test discrete_control.record.time[1] == 0.0
     t = Ribasim.datetime_since(discrete_control.record.time[2], model.config.starttime)
-    @test Date(t) == Date("2020-03-15")
+    @test Date(t) == Date("2020-03-16")
     # then the rating curve is updated to the "low" control_state
     @test only(p.tabulated_rating_curve.tables).t[2] == 1.2
-end
-
-@testitem "Setpoint with bounds control" begin
-    toml_path = normpath(
-        @__DIR__,
-        "../../generated_testmodels/level_setpoint_with_minmax/ribasim.toml",
-    )
-    @test ispath(toml_path)
-    model = Ribasim.run(toml_path)
-    p = model.integrator.p
-    (; discrete_control) = p
-    (; record, greater_than) = discrete_control
-    level = Ribasim.get_storages_and_levels(model).level[1, :]
-    t = Ribasim.tsaves(model)
-
-    t_none_1 = discrete_control.record.time[2]
-    t_in = discrete_control.record.time[3]
-    t_none_2 = discrete_control.record.time[4]
-
-    level_min = greater_than[1][1]
-    setpoint = greater_than[1][2]
-
-    t_1_none_index = findfirst(>=(t_none_1), t)
-    t_in_index = findfirst(>=(t_in), t)
-    t_2_none_index = findfirst(>=(t_none_2), t)
-
-    @test record.control_state == ["out", "none", "in", "none"]
-    @test level[t_1_none_index] <= setpoint
-    @test level[t_in_index] >= level_min
-    @test level[t_2_none_index] <= setpoint
 end
 
 @testitem "Set PID target with DiscreteControl" begin

commit 27b8c44cf6ff983dbe9998948db009f769d21860

Remove dutch_waterways model (#1403)
As discussed with @gijsber, this model is not worth maintaining and it
broke with https://github.com/Deltares/Ribasim/pull/1393.

python/ribasim_testmodels/ribasim_testmodels/__init__.py
@@ -36,7 +36,6 @@ from ribasim_testmodels.discrete_control import (
     tabulated_rating_curve_control_model,
 )
 from ribasim_testmodels.doc_example import local_pidcontrolled_cascade_model
-from ribasim_testmodels.dutch_waterways import dutch_waterways_model
 from ribasim_testmodels.equations import (
     linear_resistance_model,
     manning_resistance_model,
@@ -67,7 +66,6 @@ __all__ = [
     "bucket_model",
     "compound_variable_condition_model",
     "discrete_control_of_pid_control_model",
-    "dutch_waterways_model",
     "flow_boundary_time_model",
     "flow_condition_model",
     "flow_demand_model",

python/ribasim_testmodels/ribasim_testmodels/dutch_waterways.py
@@ -1,258 +1,0 @@
-import numpy as np
-import pandas as pd
-from ribasim.config import Node
-from ribasim.model import Model
-from ribasim.nodes import (
-    basin,
-    discrete_control,
-    flow_boundary,
-    level_boundary,
-    linear_resistance,
-    pid_control,
-    pump,
-    tabulated_rating_curve,
-)
-from shapely.geometry import Point
-
-
-def dutch_waterways_model() -> Model:
-    """Set up a model that is representative of the main Dutch rivers."""
-
-    model = Model(
-        starttime="2020-01-01",
-        endtime="2021-01-01",
-        crs="EPSG:28992",
-    )
-
-    profile_level = np.array([1.86, 3.21, 4.91, 6.61, 8.31, 10.07, 10.17, 10.27, 11.61, 12.94, 13.05, 13.69, 14.32, 14.96, 15.59])  # fmt: skip
-    width = np.array([10.0, 88.0, 137.0, 139.0, 141.0, 219.0, 220.0, 221.0, 302.0, 606.0, 837.0, 902.0, 989.0, 1008.0, 1011.0])  # fmt: skip
-    basin_profile = basin.Profile(level=profile_level, area=1e4 * width)
-
-    linear_resistance_shared = [linear_resistance.Static(resistance=[1e-2])]
-
-    # Flow rate curve from sine series
-    n_times = 250
-    time = pd.date_range(start="2020-01-01", end="2021-01-01", periods=n_times).astype(
-        "datetime64[s]"
-    )
-    flow_rate = np.zeros(n_times)
-    x = np.linspace(0, 1, n_times)
-    n_terms = 5
-    for i in np.arange(1, 2 * n_terms, 2):
-        flow_rate += 4 / (i * np.pi) * np.sin(2 * i * np.pi * x)
-
-    # Scale to desired magnitude
-    b = (250 + 800) / 2
-    a = 800 - b
-    flow_rate = a * flow_rate + b
-
-    model.flow_boundary.add(
-        Node(1, Point(196212, 436220)),
-        [flow_boundary.Time(time=time, flow_rate=flow_rate)],
-    )
-    model.basin.add(
-        Node(2, Point(194292, 439828), name="IJsselkop"),
-        [basin.State(level=[8.31]), basin_profile],
-    )
-    model.linear_resistance.add(
-        Node(3, Point(195237, 442337)), linear_resistance_shared
-    )
-    model.linear_resistance.add(
-        Node(4, Point(187489, 442384)), linear_resistance_shared
-    )
-    model.basin.add(
-        Node(5, Point(199198, 445557), name="IJssel Westervoort"),
-        [basin.State(level=[7.5]), basin_profile],
-    )
-    model.basin.add(
-        Node(6, Point(184906, 441999), name="Nederrijn Arnhem"),
-        [basin.State(level=[7.5]), basin_profile],
-    )
-    model.level_boundary.add(
-        Node(7, Point(208272, 454422)), [level_boundary.Static(level=[3.0])]
-    )
-    model.tabulated_rating_curve.add(
-        Node(8, Point(183819, 442076), name="Driel open"),
-        [
-            tabulated_rating_curve.Static(
-                control_state=["pump_low", "pump_high", "rating_curve", "rating_curve"],
-                active=[False, False, True, True],
-                # The level and flow rate for "pump_low", "pump_high" are irrelevant
-                # since the rating curve is not active here
-                level=[0.0, 0.0, 7.45, 7.46],
-                flow_rate=[0.0, 0.0, 418, 420.15],
-            )
-        ],
-    )
-    model.pump.add(
-        Node(9, Point(183859, 441561), name="Driel gecontroleerd"),
-        [
-            pump.Static(
-                active=[True, True, False],
-                control_state=["pump_low", "pump_high", "rating_curve"],
-                flow_rate=[15.0, 25.0, 1.0],
-            )
-        ],
-    )
-    model.basin.add(
-        Node(10, Point(178532, 442187)), [basin.State(level=[7.0]), basin_profile]
-    )
-    model.linear_resistance.add(
-        Node(11, Point(170775, 439396)), linear_resistance_shared
-    )
-    model.basin.add(
-        Node(12, Point(162995, 443270)), [basin.State(level=[6.0]), basin_profile]
-    )
-    model.tabulated_rating_curve.add(
-        Node(13, Point(156421, 443460), name="Amerongen open"),
-        [tabulated_rating_curve.Static(level=[4.45, 4.46], flow_rate=[418, 420.15])],
-    )
-    model.pump.add(
-        Node(14, Point(156882, 441920), name="Amerongen gecontroleerd"),
-        [pump.Static(flow_rate=[1.0], min_flow_rate=0.0, max_flow_rate=50.0)],
-    )
-    model.basin.add(
-        Node(15, Point(154547, 442540)), [basin.State(level=[5.5]), basin_profile]
-    )
-    model.level_boundary.add(
-        Node(16, Point(151776, 441465), name="Kruising ARK"),
-        [level_boundary.Static(level=[3.0])],
-    )
-    model.discrete_control.add(
-        Node(17, Point(183612, 441833), name="Controller Driel"),
-        [
-            discrete_control.Variable(
-                listen_node_type="FlowBoundary",
-                listen_node_id=[1],
-                variable="flow_rate",
-                compound_variable_id=1,
-            ),
-            discrete_control.Condition(
-                greater_than=[250, 275, 750, 800],
-                compound_variable_id=1,
-            ),
-            discrete_control.Logic(
-                truth_state=["FFFF", "U***", "T**F", "***D", "TTTT"],
-                control_state=[
-                    "pump_low",
-                    "pump_low",
-                    "pump_high",
-                    "rating_curve",
-                    "rating_curve",
-                ],
-            ),
-        ],
-    )
-    model.linear_resistance.add(
-        Node(18, Point(201507, 447443)), linear_resistance_shared
-    )
-    model.linear_resistance.add(
-        Node(19, Point(153039, 442444)), linear_resistance_shared
-    )
-    model.pid_control.add(
-        Node(20, Point(155961, 442714), name="Controller Amerongen"),
-        [
-            pid_control.Static(
-                listen_node_type="Basin",
-                listen_node_id=[12],
-                target=6.0,
-                proportional=-0.005,
-                integral=0.0,
-                derivative=-0.002,
-            )
-        ],
-    )
-
-    model.edge.add(
-        model.flow_boundary[1],
-        model.basin[2],
-        name="Pannerdensch Kanaal",
-    )
-    model.edge.add(
-        model.basin[2],
-        model.linear_resistance[3],
-        name="Start IJssel",
-    )
-    model.edge.add(
-        model.linear_resistance[3],
-        model.basin[5],
-    )
-    model.edge.add(
-        model.basin[2],
-        model.linear_resistance[4],
-        name="Start Nederrijn",
-    )
-    model.edge.add(
-        model.linear_resistance[4],
-        model.basin[6],
-    )
-    model.edge.add(
-        model.basin[6],
-        model.pump[9],
-    )
-    model.edge.add(
-        model.pump[9],
-        model.basin[10],
-    )
-    model.edge.add(
-        model.basin[10],
-        model.linear_resistance[11],
-    )
-    model.edge.add(
-        model.linear_resistance[11],
-        model.basin[12],
-    )
-    model.edge.add(
-        model.basin[12],
-        model.pump[14],
-    )
-    model.edge.add(
-        model.pump[14],
-        model.basin[15],
-    )
-    model.edge.add(
-        model.basin[6],
-        model.tabulated_rating_curve[8],
-    )
-    model.edge.add(
-        model.tabulated_rating_curve[8],
-        model.basin[10],
-    )
-    model.edge.add(
-        model.basin[12],
-        model.tabulated_rating_curve[13],
-    )
-    model.edge.add(
-        model.tabulated_rating_curve[13],
-        model.basin[15],
-    )
-    model.edge.add(
-        model.basin[5],
-        model.linear_resistance[18],
-    )
-    model.edge.add(
-        model.linear_resistance[18],
-        model.level_boundary[7],
-    )
-    model.edge.add(
-        model.basin[15],
-        model.linear_resistance[19],
-    )
-    model.edge.add(
-        model.linear_resistance[19],
-        model.level_boundary[16],
-    )
-    model.edge.add(
-        model.pid_control[20],
-        model.pump[14],
-    )
-    model.edge.add(
-        model.discrete_control[17],
-        model.tabulated_rating_curve[8],
-    )
-    model.edge.add(
-        model.discrete_control[17],
-        model.pump[9],
-    )
-
-    return model