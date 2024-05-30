1ffef08a77497c2e7e35054f8d1c22f37445941c
Martijn Visser <mgvisser@gmail.com>
2024-05-30 13:56:42 +0200

    wip

--- core/test/run_models_test.jl
+++ core/test/run_models_test.jl
@@ -128,8 +128,8 @@
     @testset "Results values" begin
         @test flow.time[1] == DateTime(2020)
         @test coalesce.(flow.edge_id[1:2], -1) == [0, 1]
-        @test flow.from_node_id[1:2] == [6, 0]
-        @test flow.to_node_id[1:2] == [0, 922]
+        @test flow.from_node_id[1:2] == [6, 6]
+        @test flow.to_node_id[1:2] == [6, 2147483647]
 
         @test basin.storage[1] ≈ 1.0
         @test basin.level[1] ≈ 0.044711584
