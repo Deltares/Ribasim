c0593fd4d44a4e4977da6c2da0b1469662d77afb
Daniel Tollenaar <daniel@d2hydro.nl>
2024-06-17 21:42:32 +0200

    Update nodes.py

--- ribasim_qgis/core/nodes.py
+++ ribasim_qgis/core/nodes.py
@@ -57,18 +57,15 @@ class Input(abc.ABC):
 
     @classmethod
     @abc.abstractmethod
-    def input_type(cls) -> str:
-        ...
+    def input_type(cls) -> str: ...
 
     @classmethod
     @abc.abstractmethod
-    def geometry_type(cls) -> str:
-        ...
+    def geometry_type(cls) -> str: ...
 
     @classmethod
     @abc.abstractmethod
-    def attributes(cls) -> list[QgsField]:
-        ...
+    def attributes(cls) -> list[QgsField]: ...
 
     @classmethod
     def is_spatial(cls):
@@ -322,7 +319,7 @@ class Edge(Input):
             marker_symbol.changeSymbolLayer(0, arrow_marker)
             marker_line_symbol_layer = cast(
                 QgsMarkerLineSymbolLayer,
-                QgsMarkerLineSymbolLayer.create({"placements": "SegmentCenter"}),
+                QgsMarkerLineSymbolLayer.create({"placements": "CentralPoint"}),
             )
 
             marker_line_symbol_layer.setSubSymbol(marker_symbol)
