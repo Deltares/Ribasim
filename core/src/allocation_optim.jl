2fb6a2a92dd342ea07dd82c4a082295fe5875ace
Bart de Koning <bart.dekoning@deltares.nl>
2024-06-25 16:36:22 +0200

    Add directly allocated flow to flow output

--- core/src/allocation_optim.jl
+++ core/src/allocation_optim.jl
@@ -776,10 +776,9 @@ function save_allocation_flows!(
     priority::Int32,
     optimization_type::OptimizationType.T,
 )::Nothing
-    (; problem, subnetwork_id, capacity) = allocation_model
+    (; flow_priority, problem, subnetwork_id, capacity) = allocation_model
     (; allocation, graph) = p
     (; record_flow) = allocation
-    F = problem[:F]
     F_basin_in = problem[:F_basin_in]
     F_basin_out = problem[:F_basin_out]
 
@@ -798,12 +797,12 @@ function save_allocation_flows!(
         flow_rate = 0.0
 
         if haskey(graph, edge_1...)
-            flow_rate += JuMP.value(F[edge_1])
+            flow_rate += flow_priority[edge_1]
             sign_2 = -1.0
             edge_metadata = graph[edge_1...]
         else
             edge_1_reverse = reverse(edge_1)
-            flow_rate -= JuMP.value(F[edge_1_reverse])
+            flow_rate -= flow_priority[edge_1_reverse]
             sign_2 = 1.0
             edge_metadata = graph[edge_1_reverse...]
         end
@@ -813,7 +812,7 @@ function save_allocation_flows!(
         if edge_2 == reverse(edge_1) &&
            !(edge_1[1].type == NodeType.UserDemand || edge_1[2].type == NodeType.UserDemand)
             # If so, these edges are both processed in this iteration
-            flow_rate += sign_2 * JuMP.value(F[edge_2])
+            flow_rate += sign_2 * flow_priority[edge_2]
             skip = true
         end
 
@@ -855,6 +854,7 @@ end
 
 function allocate_to_users_from_connected_basin!(
     allocation_model::AllocationModel,
+    flow_priority,
     p::Parameters,
     priority_idx::Int,
 )::Nothing
@@ -883,6 +883,9 @@ function allocate_to_users_from_connected_basin!(
             # Subtract the allocated amount from the user demand and basin capacity
             user_demand.demand_reduced[node_id.idx, priority_idx] -= allocated
             JuMP.set_normalized_rhs(constraint, capacity - allocated)
+
+            # Add the allocated flow
+            flow_priority[(upstream_basin_id, node_id)] += allocated
         end
     end
 
@@ -897,9 +900,14 @@ function optimize_priority!(
     priority_idx::Int,
     optimization_type::OptimizationType.T,
 )::Nothing
-    (; problem) = allocation_model
+    (; problem, flow_priority) = allocation_model
     (; allocation) = p
     (; priorities) = allocation
+
+    # Start the values of the flows at this priority at 0.0
+    for edge in keys(flow_priority.data)
+        flow_priority[edge] = 0.0
+    end
 
     set_capacities_flow_demand_outflow!(allocation_model, p, priority_idx)
 
@@ -912,7 +920,12 @@ function optimize_priority!(
 
     # Allocate to UserDemand nodes from the directly connected basin
     # This happens outside the JuMP optimization
-    allocate_to_users_from_connected_basin!(allocation_model, p, priority_idx)
+    allocate_to_users_from_connected_basin!(
+        allocation_model,
+        flow_priority,
+        p,
+        priority_idx,
+    )
 
     # Solve the allocation problem for this priority
     JuMP.optimize!(problem)
@@ -923,6 +936,11 @@ function optimize_priority!(
         error(
             "Allocation of subnetwork $subnetwork_id, priority $priority coudn't find optimal solution.",
         )
+    end
+
+    # Add the values of the flows at this priority
+    for edge in only(problem[:F].axes)
+        flow_priority[edge] += JuMP.value(problem[:F][edge])
     end
 
     # Assign the allocations to the UserDemand for this priority
