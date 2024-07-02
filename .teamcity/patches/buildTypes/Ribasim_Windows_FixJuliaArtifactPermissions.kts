e5109109e11e1bd7b435d2e8baa92022df092801
git@evetion.nl <git@evetion.nl@TeamCity>
2024-07-02 20:38:41 +0200

    TeamCity change in 'Ribasim / Ribasim_Windows' project: runners of 'Fix Julia Artifact permissions' build configuration were updated

--- .teamcity/patches/buildTypes/Ribasim_Windows_FixJuliaArtifactPermissions.kts
+++ .teamcity/patches/buildTypes/Ribasim_Windows_FixJuliaArtifactPermissions.kts
@@ -2,6 +2,7 @@ package patches.buildTypes
 
 import jetbrains.buildServer.configs.kotlin.*
 import jetbrains.buildServer.configs.kotlin.BuildType
+import jetbrains.buildServer.configs.kotlin.buildSteps.script
 import jetbrains.buildServer.configs.kotlin.ui.*
 
 /*
@@ -13,5 +14,13 @@ create(RelativeId("Ribasim_Windows"), BuildType({
     id("Ribasim_Windows_FixJuliaArtifactPermissions")
     name = "Fix Julia Artifact permissions"
     description = "Temporary build to run on a failing agent"
+
+    steps {
+        script {
+            name = "Reset permissions"
+            id = "Reset_permissions"
+            scriptContent = "icacls %env.HOMEPATH%/.julia/artifacts /q /c /t /reset"
+        }
+    }
 }))
 
