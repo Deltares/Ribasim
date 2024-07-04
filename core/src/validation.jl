dacf739b8c6dd4dcbcc22aa48a2827e811a97468
Jingru Feng <feng_jingru@foxmail.com>
2024-07-04 11:57:51 +0200

    min_crest_level default to basin_bottom level

--- core/src/validation.jl
+++ core/src/validation.jl
@@ -448,7 +448,9 @@ function valid_outlet_crest_level(graph::MetaGraph, outlet::Outlet, basin::Basin
         if id_in.type == NodeType.Basin
             #validation:
             basin_bottom_level = basin_bottom(basin, id_in)
-            if crest <= basin_bottom_level[2]
+            if crest == -Inf
+                crest == basin_bottom_level[2]
+            else if crest <= basin_bottom_level[2]
                 @error "Minimum crest level of outlet $id is lower than bottom of upstream basin $id_in"
                 errors = true
             end
