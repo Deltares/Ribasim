6af395feb6db59e068e13b1272589f4e665d20f3
Martijn Visser <mgvisser@gmail.com>
2024-07-16 15:53:42 +0200

    Require QGIS 3.34 (LTR); remove activation scripts

--- ribasim_qgis/widgets/ribasim_widget.py
+++ ribasim_qgis/widgets/ribasim_widget.py
@@ -13,9 +13,9 @@ from typing import Any, cast
 
 from PyQt5.QtWidgets import QTabWidget, QVBoxLayout, QWidget
 from qgis.core import (
+    Qgis,
     QgsAbstractVectorLayerLabeling,
     QgsCoordinateReferenceSystem,
-    QgsEditFormConfig,
     QgsFeatureRenderer,
     QgsLayerTreeGroup,
     QgsMapLayer,
@@ -173,11 +173,10 @@ class RibasimWidget(QWidget):
         assert maplayer is not None
         if suppress is not None:
             config = maplayer.editFormConfig()
-            # From QGIS 3.32 on we can use https://github.com/Deltares/Ribasim/commit/8a22fc0630f343069fd3c285ae46e9fde0c71a32
             config.setSuppress(
-                QgsEditFormConfig.FeatureFormSuppress.SuppressOn  # type: ignore
+                Qgis.AttributeFormSuppression.On
                 if suppress
-                else QgsEditFormConfig.FeatureFormSuppress.SuppressDefault  # type: ignore
+                else Qgis.AttributeFormSuppression.Default
             )
             maplayer.setEditFormConfig(config)
         if renderer is not None:
