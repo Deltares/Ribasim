2853e59766854a98df4bf97cff56810fb14a194f
Martijn Visser <mgvisser@gmail.com>
2024-07-17 10:57:05 +0200

    Require QGIS 3.34 (LTR); remove activation scripts (#1627)
    Currently the only thing we use from a newer QGIS (3.32) is
    `Qgis.AttributeFormSuppression.On`. So we could technically still
    support older versions. Though since 3.34 is the new Long Term Release,
    and we have yet to release v1.0, it is best to make it clear what we'll
    support now.
    
    This also removes the activation scripts, including `open-vscode.bat`.
    With [this VSCode Python
    release](https://github.com/microsoft/vscode-python/releases/tag/v2024.10.0),
    it now correctly locates pixi environments. I added instructions on this
    in the dev docs.
    
    The mypy settings still affected 4 issues in the QGIS Python code. All
    are of this type:
    ```
    ribasim_qgis\ribasim_qgis.py:11: error: Class cannot subclass "QgsDockWidget" (has type "Any")  [misc]
    ```
    I added type ignores for those four, I don't think this disables much
    effective functionality since mypy still finds issues inside the class.

--- utils/env_setup.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-QUARTO_PYTHON=python
-export QUARTO_PYTHON
-relative_conda_prefix=$(realpath --relative-to="$PWD" "$CONDA_PREFIX")
-MYPYPATH=$relative_conda_prefix/share/qgis/python
-export MYPYPATH
