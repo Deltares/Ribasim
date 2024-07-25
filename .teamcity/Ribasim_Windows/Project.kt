14b71be88f84678be3d3e24c776eefb124fdc9a1
Sunny Titus <sunny.titus@deltares.nl>
2024-07-25 12:52:47 +0200

    Add composite build to windows build

--- .teamcity/Ribasim_Windows/Project.kt
+++ .teamcity/Ribasim_Windows/Project.kt
@@ -9,7 +9,54 @@ object Project : Project({
     id("Ribasim_Windows")
     name = "Ribasim_Windows"
 
+    buildType(Main)
     buildType(Windows_BuildRibasim)
     buildType(Windows_TestDelwaqCoupling)
     buildType(Windows_TestRibasimBinaries)
+})
+
+object Main : BuildType({
+    name = "RibasimMain"
+
+    allowExternalStatus = true
+    type = Type.COMPOSITE
+
+    vcs {
+        root(RibasimVcs, ". => ribasim")
+    }
+
+    triggers {
+        vcs {
+        }
+    }
+
+    features {
+        commitStatusPublisher {
+            vcsRootExtId = "${RibasimVcs.id}"
+            publisher = github {
+                githubUrl = "https://api.github.com"
+                authType = personalToken {
+                    token = "credentialsJSON:6b37af71-1f2f-4611-8856-db07965445c0"
+                }
+            }
+        }
+        pullRequests {
+            vcsRootExtId = "${RibasimVcs.id}"
+            provider = github {
+                authType = token {
+                    token = "credentialsJSON:6b37af71-1f2f-4611-8856-db07965445c0"
+                }
+                filterAuthorRole = PullRequests.GitHubRoleFilter.MEMBER
+            }
+        }
+    }
+
+    dependencies {
+        snapshot(Windows_TestRibasimBinaries) {
+            onDependencyFailure = FailureAction.FAIL_TO_START
+        }
+        snapshot(Windows_TestDelwaqCoupling) {
+            onDependencyFailure = FailureAction.FAIL_TO_START
+        }
+    }
 })
