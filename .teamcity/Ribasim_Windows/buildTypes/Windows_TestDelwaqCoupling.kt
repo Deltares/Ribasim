Deltares/Ribasim

commit c8f9ca5289a9b0f5e36df165136621535a9134ff

Use install-ci on CI (#1656)
These are the last two occurrences in our TeamCity config. This avoids
installing QGIS plugins and pre-commit, both of which are not needed on
TeamCity.

```toml
install = { depends_on = [
    "install-ci",
    "install-qgis-plugins",
    "install-pre-commit",
] }
```

.teamcity/Ribasim_Windows/buildTypes/Windows_TestDelwaqCoupling.kt
@@ -28,7 +28,7 @@ object Windows_TestDelwaqCoupling : BuildType({
             id = "Run_Delwaq"
             workingDir = "ribasim"
             scriptContent = """
-                pixi install
+                pixi install-ci
                 pixi run ribasim-core-testmodels basic
                 set D3D_HOME=%teamcity.build.checkoutDir%/dimr
                 pixi run delwaq

.teamcity/Ribasim_Linux/buildTypes/Linux_TestRibasimBinaries.kt
@@ -44,7 +44,7 @@ object Linux_TestRibasimBinaries : BuildType({
                 source /usr/share/Modules/init/bash
 
                 module load pixi
-                pixi run install
+                pixi run install-ci
                 pixi run test-ribasim-api
                 pixi run test-ribasim-cli
             """.trimIndent()

commit 50f96b6bdf27b0df97c7a583ceab1c45519829ed

Delwaq pixi run install-ci (#1664)
Does not fix a regression from #1656. I thought it said `pixi run
install`, but it was `pixi install`.

The Delwaq regression may also be related to
https://github.com/Deltares/Ribasim/pull/1649

.teamcity/Ribasim_Windows/buildTypes/Windows_TestDelwaqCoupling.kt
@@ -28,7 +28,7 @@ object Windows_TestDelwaqCoupling : BuildType({
             id = "Run_Delwaq"
             workingDir = "ribasim"
             scriptContent = """
-                pixi install-ci
+                pixi install
                 pixi run ribasim-core-testmodels basic
                 set D3D_HOME=%teamcity.build.checkoutDir%/dimr
                 pixi run delwaq