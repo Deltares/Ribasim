38cab4fa0f09654977249ec126bec3838e69b49b
Jingru923 <47444880+Jingru923@users.noreply.github.com>
2024-07-31 08:32:50 +0200

    Add technical documentation for allocation (#1579)
    Fixes #1540
    We now have a technical documentation for allocation!
    ---------
    
    Co-authored-by: Bart de Koning <bart.dekoning@deltares.nl>
    Co-authored-by: Bart de Koning <74617371+SouthEndMusic@users.noreply.github.com>

--- core/src/allocation_optim.jl
+++ core/src/allocation_optim.jl
@@ -40,10 +40,12 @@ function set_objective_priority!(
     (; main_network_connections, subnetwork_demands) = allocation
     F = problem[:F]
 
+    # Initialize an empty quadratic expression for the objective
     ex = JuMP.QuadExpr()
 
-    # Terms for subnetworks as UserDemand
+    # Terms for subnetworks acting as UserDemand on the main network
     if is_main_network(subnetwork_id)
+        # Loop over the connections between main and subnetwork
         for connections_subnetwork in main_network_connections[2:end]
             for connection in connections_subnetwork
                 d = subnetwork_demands[connection][priority_idx]
@@ -53,7 +55,7 @@ function set_objective_priority!(
         end
     end
 
-    # Terms for UserDemand nodes and LevelDemand nodes
+    # Terms for UserDemand nodes and FlowDemand nodes
     for edge in keys(capacity.data)
         to_node_id = edge[2]
 
@@ -91,6 +93,7 @@ function set_objective_priority!(
         add_objective_term!(ex, d, F_ld)
     end
 
+    # Add the new objective to the problem
     new_objective = JuMP.@expression(problem, ex)
     JuMP.@objective(problem, Min, new_objective)
     return nothing
@@ -608,6 +611,8 @@ function adjust_demands!(
     F = problem[:F]
 
     for node_id in flow_demand.node_id
+
+        # Only update data for FlowDemand nodes in the current subnetwork
         if graph[node_id].subnetwork_id != subnetwork_id
             continue
         end
@@ -653,6 +658,9 @@ function adjust_capacities_buffer!(allocation_model::AllocationModel)::Nothing
 
     for node_id in only(constraints_flow_buffer.axes)
         constraint = constraints_flow_buffer[node_id]
+
+        # The capacity should not be able to get below 0, but can get to small negative numbers,
+        # probably due to floating point errors. Therefore new_capacity = max(0, new_capacity) is applied.
         buffer_capacity = max(
             0.0,
             JuMP.normalized_rhs(constraint) + JuMP.value(F_flow_buffer_in[node_id]) -
@@ -667,6 +675,10 @@ end
 Set the capacity of the outflow edge from a node with a flow demand:
 - To Inf if the current priority is other than the priority of the flow demand
 - To 0.0 if the current priority is equal to the priority of the flow demand
+
+This is done so that flow can go towards the node with the flow demand into its buffer,
+to avoid the problem that the flow has nowhere to go after this node and to make sure
+that this flow can be used for later priorities
 """
 function set_capacities_flow_demand_outflow!(
     allocation_model::AllocationModel,
@@ -1073,10 +1085,6 @@ function allocate_demands!(
     (; allocation) = p
     (; subnetwork_id) = allocation_model
     (; priorities) = allocation
-
-    if is_main_network(subnetwork_id)
-        @assert optimization_type == OptimizationType.allocate "For the main network no demands have to be collected"
-    end
 
     set_initial_capacities_inlet!(allocation_model, p, optimization_type)
 
