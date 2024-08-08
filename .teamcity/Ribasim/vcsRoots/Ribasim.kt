752c1c5e5ecfc4d315629a31c5ee01fff57d5692
Jingru Feng <feng_jingru@foxmail.com>
2024-08-08 17:48:47 +0200

    update vcs

--- .teamcity/Ribasim/vcsRoots/Ribasim.kt
+++ .teamcity/Ribasim/vcsRoots/Ribasim.kt
@@ -5,15 +5,21 @@ import jetbrains.buildServer.configs.kotlin.vcs.GitVcsRoot
 object Ribasim : GitVcsRoot({
     name = "Ribasim"
     url = "https://github.com/Deltares/Ribasim"
-    branch = "main"
-    branchSpec = """
-        +:refs/heads/main
-        +:refs/tags/*
-        +:refs/heads/gh-readonly-queue/*
-    """.trimIndent()
-    useTagsAsBranches = true
-    authMethod = password {
-        userName = "teamcity-deltares"
-        password = "credentialsJSON:abf605ce-e382-4b10-b5de-8a7640dc58d9"
-    }
+    branch = "bench-large-model"
+    branchSpec = "+:refs/heads/*"
 })
+//object Ribasim : GitVcsRoot({
+//    name = "Ribasim"
+//    url = "https://github.com/Deltares/Ribasim"
+//    branch = "main"
+//    branchSpec = """
+//        +:refs/heads/main
+//        +:refs/tags/*
+//        +:refs/heads/gh-readonly-queue/*
+//    """.trimIndent()
+//    useTagsAsBranches = true
+//    authMethod = password {
+//        userName = "teamcity-deltares"
+//        password = "credentialsJSON:abf605ce-e382-4b10-b5de-8a7640dc58d9"
+//    }
+//})
