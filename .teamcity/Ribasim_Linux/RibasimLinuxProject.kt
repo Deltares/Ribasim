2cb681a4257ff1918174d7b9884af34edeb32306
Jingru923 <47444880+Jingru923@users.noreply.github.com>
2024-08-13 16:57:30 +0200

    no more Zip bomb from built binaries (#1722)
    Fixes #1716

--- .teamcity/Ribasim_Linux/RibasimLinuxProject.kt
+++ .teamcity/Ribasim_Linux/RibasimLinuxProject.kt
@@ -52,7 +52,7 @@ object Linux_BuildRibasim : BuildType({
 
     name = "Build Ribasim"
 
-    artifactRules = """ribasim\build\ribasim => ribasim_linux.zip"""
+    artifactRules = """ribasim\build\ribasim => ribasim_linux.zip!/ribasim"""
 })
 
 object Linux_TestRibasimBinaries : BuildType({
@@ -68,7 +68,7 @@ object Linux_TestRibasimBinaries : BuildType({
                 id = "ARTIFACT_DEPENDENCY_570"
                 cleanDestination = true
                 artifactRules = """
-                    ribasim_linux.zip!** => ribasim/build/ribasim
+                    ribasim_linux.zip!/ribasim/** => ribasim/build/ribasim
                 """.trimIndent()
             }
         }
--- .teamcity/Ribasim_Windows/RibasimWindowsProject.kt
+++ .teamcity/Ribasim_Windows/RibasimWindowsProject.kt
@@ -62,7 +62,7 @@ object Windows_BuildRibasim : BuildType({
             scriptContent = "pixi run add-ribasim-icon"
         }
     }
-    artifactRules = """ribasim\build\ribasim => ribasim_windows.zip"""
+    artifactRules = """ribasim\build\ribasim => ribasim_windows.zip!/ribasim"""
 })
 
 object Windows_TestRibasimBinaries : BuildType({
@@ -78,7 +78,7 @@ object Windows_TestRibasimBinaries : BuildType({
                 id = "ARTIFACT_DEPENDENCY_570"
                 cleanDestination = true
                 artifactRules = """
-                    ribasim_windows.zip!** => ribasim/build/ribasim
+                    ribasim_windows.zip!/ribasim/** => ribasim/build/ribasim
                 """.trimIndent()
             }
         }
