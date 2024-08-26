6f757ef9a35da85eac8218ac3b56701ad9f319b0
Maarten Pronk <git@evetion.nl>
2024-08-26 20:55:26 +0200

    Be kinder in our warnings.

--- python/ribasim/ribasim/migrations.py
+++ python/ribasim/ribasim/migrations.py
@@ -5,8 +5,8 @@ from pandas import DataFrame
 
 
 def nodeschemamigration(gdf: GeoDataFrame) -> GeoDataFrame:
-    warnings.warn("Migrating outdated Node table.", UserWarning)
     if "node_id" in gdf.columns:
+        warnings.warn("Migrating outdated Node table.", UserWarning)
         assert gdf["node_id"].is_unique, "Node IDs have to be unique."
         gdf.set_index("node_id", inplace=True)
 
@@ -14,29 +14,30 @@ def nodeschemamigration(gdf: GeoDataFrame) -> GeoDataFrame:
 
 
 def edgeschemamigration(gdf: GeoDataFrame) -> GeoDataFrame:
-    warnings.warn("Migrating outdated Edge table.", UserWarning)
     if "from_node_type" in gdf.columns:
+        warnings.warn("Migrating outdated Edge table.", UserWarning)
         gdf.drop("from_node_type", inplace=True, axis=1)
     if "to_node_type" in gdf.columns:
+        warnings.warn("Migrating outdated Edge table.", UserWarning)
         gdf.drop("to_node_type", inplace=True, axis=1)
     if "edge_id" in gdf.columns:
+        warnings.warn("Migrating outdated Edge table.", UserWarning)
         gdf.set_index("edge_id", inplace=True)
 
     return gdf
 
 
 def basinstaticschemamigration(df: DataFrame) -> DataFrame:
-    warnings.warn("Migrating outdated Basin / Static table.", UserWarning)
     if "urban_runoff" in df.columns:
-        print(df)
+        warnings.warn("Migrating outdated Basin / Static table.", UserWarning)
         df.drop("urban_runoff", inplace=True, axis=1)
 
     return df
 
 
 def basintimeschemamigration(df: DataFrame) -> DataFrame:
-    warnings.warn("Migrating outdated Basin / Time table.", UserWarning)
     if "urban_runoff" in df.columns:
+        warnings.warn("Migrating outdated Basin / Time table.", UserWarning)
         df.drop("urban_runoff", inplace=True, axis=1)
 
     return df
