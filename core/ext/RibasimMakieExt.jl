3ff1ba8ff7015e8e82b462a4b68a66fea397a123
Bart de Koning <bart.dekoning@deltares.nl>
2024-08-28 18:34:01 +0200

    Refactor Makie extension to work with breaking changes on edge data

--- core/ext/RibasimMakieExt.jl
+++ core/ext/RibasimMakieExt.jl
@@ -1,6 +1,6 @@
 module RibasimMakieExt
 using DataFrames: DataFrame
-using Makie: Figure, Axis, lines!, axislegend
+using Makie: Figure, Axis, lines!, scatter!, axislegend
 using Ribasim: Ribasim, Model
 
 function Ribasim.plot_basin_data!(model::Model, ax::Axis, column::Symbol)
@@ -8,6 +8,7 @@ function Ribasim.plot_basin_data!(model::Model, ax::Axis, column::Symbol)
     for node_id in unique(basin_data.node_id)
         group = filter(:node_id => ==(node_id), basin_data)
         lines!(ax, group.time, getproperty(group, column); label = "Basin #$node_id")
+        scatter!(ax, group.time, getproperty(group, column))
     end
 
     axislegend(ax)
@@ -23,31 +24,24 @@ function Ribasim.plot_basin_data(model::Model)
     f
 end
 
-function Ribasim.plot_flow!(
-    model::Model,
-    ax::Axis,
-    edge_id::Int32;
-    skip_conservative_out = false,
-)
+function Ribasim.plot_flow!(model::Model, ax::Axis, edge_metadata::Ribasim.EdgeMetadata)
     flow_data = DataFrame(Ribasim.flow_table(model))
-    flow_data = filter(:edge_id => ==(edge_id), flow_data)
-    first_row = first(flow_data)
-    # Skip outflows of conservative nodes because these are the same as the inflows
-    if skip_conservative_out &&
-       Ribasim.NodeType.T(first_row.from_node_type) in Ribasim.conservative_nodetypes
-        return nothing
-    end
-    label = "$(first_row.from_node_type) #$(first_row.from_node_id) → $(first_row.to_node_type) #$(first_row.to_node_id)"
+    flow_data = filter(:edge_id => ==(edge_metadata.id), flow_data)
+    label = "$(edge_metadata.edge[1]) → $(edge_metadata.edge[2])"
     lines!(ax, flow_data.time, flow_data.flow_rate; label)
+    scatter!(ax, flow_data.time, flow_data.flow_rate)
     return nothing
 end
 
-function Ribasim.plot_flow(model::Model)
+function Ribasim.plot_flow(model::Model; skip_conservative_out = true)
     f = Figure()
     ax = Axis(f[1, 1]; xlabel = "time", ylabel = "flow rate [m³s⁻¹]")
-    edge_ids = unique(Ribasim.flow_table(model).edge_id)
-    for edge_id in edge_ids
-        Ribasim.plot_flow!(model, ax, edge_id; skip_conservative_out = true)
+    for edge_metadata in values(model.integrator.p.graph.edge_data)
+        if skip_conservative_out &&
+           edge_metadata.edge[1].type in Ribasim.conservative_nodetypes
+            continue
+        end
+        Ribasim.plot_flow!(model, ax, edge_metadata)
     end
     axislegend(ax)
     f
