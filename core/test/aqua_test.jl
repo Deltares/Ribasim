775dd76b9e1f77580a93bfb31caafa785f0d36b5
Bart de Koning <bart.dekoning@deltares.nl>
2024-09-03 13:28:23 +0200

    Get rid of type piracy

--- core/test/aqua_test.jl
+++ core/test/aqua_test.jl
@@ -1,12 +1,5 @@
 @testitem "Aqua" begin
     import Aqua
     using DataInterpolations: AbstractInterpolation
-    Aqua.test_all(
-        Ribasim;
-        ambiguities = false,
-        persistent_tasks = false,
-        # TODO: Remove AbstractInterpolation exception when DataInterpolations
-        # is supported in SparseConnectivityTracer
-        piracies = (treat_as_own = [AbstractInterpolation],),
-    )
+    Aqua.test_all(Ribasim; ambiguities = false, persistent_tasks = false)
 end
