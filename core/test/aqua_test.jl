bc32447b290da05007f87cae945dd9d03cb7ab50
Bart de Koning <bart.dekoning@deltares.nl>
2024-09-04 08:58:34 +0200

    Make aqua happy

--- core/test/aqua_test.jl
+++ core/test/aqua_test.jl
@@ -1,4 +1,11 @@
 @testitem "Aqua" begin
     import Aqua
-    Aqua.test_all(Ribasim; ambiguities = false, persistent_tasks = false)
+    using FindFirstFunctions: Guesser
+    Aqua.test_all(
+        Ribasim;
+        ambiguities = false,
+        persistent_tasks = false,
+        # TODO: Remove after https://github.com/SciML/FindFirstFunctions.jl/pull/26
+        piracies = (treat_as_own = [Guesser],),
+    )
 end
