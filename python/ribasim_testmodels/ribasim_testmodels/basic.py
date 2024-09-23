5ea17fb75ca514c2fa03199daf827d7029eec39e
Martijn Visser <mgvisser@gmail.com>
2024-09-23 17:12:36 +0200

    Ensure timestamps are equal

--- python/ribasim_testmodels/ribasim_testmodels/basic.py
+++ python/ribasim_testmodels/ribasim_testmodels/basic.py
@@ -292,7 +292,7 @@ def tabulated_rating_curve_model() -> ribasim.Model:
                 time=[
                     # test millisecond precision
                     pd.Timestamp("2020-01-01 00:00:00.001"),
-                    pd.Timestamp("2020-01"),
+                    pd.Timestamp("2020-01-01 00:00:00.001"),
                     pd.Timestamp("2020-02"),
                     pd.Timestamp("2020-02"),
                     pd.Timestamp("2020-03"),
