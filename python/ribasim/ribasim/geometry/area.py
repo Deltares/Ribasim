c10a25a5b6f30ada2f65fab499c1fa236e2bc2b4
Maarten Pronk <git@evetion.nl>
2024-10-07 20:32:45 +0200

    Allow Polygon geometry for Basin / Area.

--- python/ribasim/ribasim/geometry/area.py
+++ python/ribasim/ribasim/geometry/area.py
@@ -2,7 +2,7 @@ import pandera as pa
 from pandera.dtypes import Int32
 from pandera.typing import Index, Series
 from pandera.typing.geopandas import GeoSeries
-from shapely.geometry import MultiPolygon
+from shapely.geometry import MultiPolygon, Polygon
 
 from .base import _GeoBaseSchema
 
@@ -11,3 +11,9 @@ class BasinAreaSchema(_GeoBaseSchema):
     fid: Index[Int32] = pa.Field(default=0, check_name=True)
     node_id: Series[Int32] = pa.Field(nullable=False, default=0)
     geometry: GeoSeries[MultiPolygon] = pa.Field(default=None, nullable=True)
+
+    @pa.parser("geometry")
+    def convert_to_multi(cls, series):
+        return series.apply(
+            lambda geom: MultiPolygon([geom]) if isinstance(geom, Polygon) else geom
+        )
