74e4ed0034a9f956f126092e677fb3bbaa34ec1f
Martijn Visser <mgvisser@gmail.com>
2024-10-07 20:05:19 +0200

    Set abstol to 1e-7 also

--- python/ribasim/ribasim/config.py
+++ python/ribasim/ribasim/config.py
@@ -99,7 +99,7 @@ class Solver(ChildModel):
         If a smaller dt than dtmin is needed to meet the set error tolerances, the simulation stops, unless force_dtmin = true
         (Optional, defaults to False)
     abstol : float
-        The absolute tolerance for adaptive timestepping (Optional, defaults to 1e-6)
+        The absolute tolerance for adaptive timestepping (Optional, defaults to 1e-7)
     reltol : float
         The relative tolerance for adaptive timestepping (Optional, defaults to 1e-7)
     maxiters : int
