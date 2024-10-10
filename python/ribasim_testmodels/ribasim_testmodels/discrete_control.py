88cc1c1032b7ff29d97fc264c915bb586a58f6e3
Maarten Pronk <git@evetion.nl>
2024-10-10 10:09:11 +0200

    Making models is hard.

--- python/ribasim_testmodels/ribasim_testmodels/discrete_control.py
+++ python/ribasim_testmodels/ribasim_testmodels/discrete_control.py
@@ -601,18 +601,19 @@ def continuous_concentration_condition_model() -> Model:
 
     model = Model(
         starttime="2020-01-01",
-        endtime="2021-01-01",
+        endtime="2020-02-01",
         crs="EPSG:28992",
+        solver=Solver(saveat=86400 / 8),
     )
 
     basi = model.basin.add(
         Node(1, Point(0, 0)),
         [
-            basin.Profile(area=1000.0, level=[0.0, 1.0]),
-            basin.State(level=[20.0]),
+            basin.Profile(area=[10.0, 100.0], level=[0.0, 1.0]),
+            basin.State(level=[10.0]),
             basin.ConcentrationState(
                 substance=["Cl"],
-                concentration=[1.0],  # start of slightly salty
+                concentration=[35.0],  # start of slightly salty
             ),
             basin.Concentration(
                 time=pd.date_range(
@@ -629,9 +630,8 @@ def continuous_concentration_condition_model() -> Model:
         [
             linear_resistance.Static(
                 control_state=["On", "Off"],
-                # active=[True, False],
-                resistance=[0.001, 100],
-                max_flow_rate=[0.11, 0.0],
+                resistance=[0.001, 10],
+                max_flow_rate=[0.2, 0.0001],
             )
         ],
     )
@@ -639,7 +639,7 @@ def continuous_concentration_condition_model() -> Model:
     levelb = model.level_boundary.add(
         Node(3, Point(-2, 0)),
         [
-            level_boundary.Static(level=[25.0]),
+            level_boundary.Static(level=[35.0]),
             level_boundary.Concentration(
                 time=pd.date_range(
                     start="2020-01-01", end="2021-01-01", periods=10, unit="ms"
@@ -678,7 +678,7 @@ def continuous_concentration_condition_model() -> Model:
         ],
     )
 
-    outl = model.outlet.add(Node(6, Point(0, -0.5)), [outlet.Static(flow_rate=[0.2])])
+    outl = model.outlet.add(Node(6, Point(0, -0.5)), [outlet.Static(flow_rate=[0.11])])
     term = model.terminal.add(Node(7, Point(0, -1)))
 
     model.edge.add(levelb, linearr)
