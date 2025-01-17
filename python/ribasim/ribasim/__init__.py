fcd344eda4a54f534cbb430b91699299badcf9bb
Martijn Visser <mgvisser@gmail.com>
2025-01-17 11:56:22 +0100

    Bump version numbers to 2025.1.0

--- python/ribasim/ribasim/__init__.py
+++ python/ribasim/ribasim/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "2024.11.0"
+__version__ = "2025.1.0"
 # Keep synced write_schema_version in ribasim_qgis/core/geopackage.py
 __schema_version__ = 3
 
--- ribasim_qgis/widgets/dataset_widget.py
+++ ribasim_qgis/widgets/dataset_widget.py
@@ -337,7 +337,7 @@ class DatasetWidget(QWidget):
                     f'crs = "{self.ribasim_widget.crs.authid()}"\n',
                     'input_dir = "."\n',
                     'results_dir = "results"\n',
-                    'ribasim_version = "2024.11.0"\n',
+                    'ribasim_version = "2025.1.0"\n',
                 ]
             )
 
