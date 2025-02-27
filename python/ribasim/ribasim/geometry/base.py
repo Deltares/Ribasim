3677c12a2cd00752721f3fccac68f76c05c18ec1
Martijn Visser <mgvisser@gmail.com>
2025-02-27 15:08:13 +0100

    Coerce geometries to 2D

--- python/ribasim/ribasim/geometry/base.py
+++ python/ribasim/ribasim/geometry/base.py
@@ -2,7 +2,7 @@ from typing import Any, get_type_hints
 
 import pandera as pa
 from pandera.typing import Series
-from pandera.typing.geopandas import GeoSeries
+from pandera.typing.geopandas import GeoDataFrame, GeoSeries
 
 from ribasim.schemas import _BaseSchema
 
@@ -12,3 +12,8 @@ class _GeoBaseSchema(_BaseSchema):
     def is_correct_geometry_type(cls, geoseries: GeoSeries[Any]) -> Series[bool]:
         T = get_type_hints(cls)["geometry"].__args__[0]
         return geoseries.map(lambda geom: isinstance(geom, T))
+
+    @pa.check_types
+    def force_2d(cls, gdf: GeoDataFrame[Any]) -> GeoDataFrame[Any]:
+        gdf.geometry = gdf.geometry.force_2d()
+        return gdf
