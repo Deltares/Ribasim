8f8b85014d95612d9f17eac05cdaace7f91e6062
Martijn Visser <mgvisser@gmail.com>
2025-02-28 10:16:54 +0100

    Remove RibasimMakieExt (#2113)
    Everytime Ribasim needed to be recompiled and Makie was available we had
    to spend considerable time recompiling this extension. We avoid having
    to pay this price by moving it to `utils/plot.jl` instead.
    
    ```julia
    using Ribasim
    
    m = Ribasim.run("ribasim.toml")
    include("../utils/plot.jl")
    using CairoMakie
    plot_basin_data(m)
    ```
    
    Helps latency #1942 and we should check if this helps with #2015 (edit:
    it doesn't). CairoMakie is still in our root Manifest.toml since we use
    it in our docs.

--- utils/plot.jl
+++ utils/plot.jl
@@ -1,9 +1,10 @@
-module RibasimMakieExt
+# Utility functions to plot Ribasim results.
+
 using DataFrames: DataFrame
 using Makie: Figure, Axis, scatterlines!, axislegend
 using Ribasim: Ribasim, Model
 
-function Ribasim.plot_basin_data!(model::Model, ax::Axis, column::Symbol)
+function plot_basin_data!(model::Model, ax::Axis, column::Symbol)
     basin_data = DataFrame(Ribasim.basin_table(model))
     for node_id in unique(basin_data.node_id)
         group = filter(:node_id => ==(node_id), basin_data)
@@ -14,16 +15,16 @@ function Ribasim.plot_basin_data!(model::Model, ax::Axis, column::Symbol)
     return nothing
 end
 
-function Ribasim.plot_basin_data(model::Model)
+function plot_basin_data(model::Model)
     f = Figure()
     ax1 = Axis(f[1, 1]; ylabel = "level [m]")
     ax2 = Axis(f[2, 1]; xlabel = "time", ylabel = "storage [m³]")
-    Ribasim.plot_basin_data!(model, ax1, :level)
-    Ribasim.plot_basin_data!(model, ax2, :storage)
+    plot_basin_data!(model, ax1, :level)
+    plot_basin_data!(model, ax2, :storage)
     f
 end
 
-function Ribasim.plot_flow!(model::Model, ax::Axis, link_metadata::Ribasim.LinkMetadata)
+function plot_flow!(model::Model, ax::Axis, link_metadata::Ribasim.LinkMetadata)
     flow_data = DataFrame(Ribasim.flow_table(model))
     flow_data = filter(:link_id => ==(link_metadata.id), flow_data)
     label = "$(link_metadata.link[1]) → $(link_metadata.link[2])"
@@ -31,7 +32,7 @@ function Ribasim.plot_flow!(model::Model, ax::Axis, link_metadata::Ribasim.LinkM
     return nothing
 end
 
-function Ribasim.plot_flow(model::Model; skip_conservative_out = true)
+function plot_flow(model::Model; skip_conservative_out = true)
     f = Figure()
     ax = Axis(f[1, 1]; xlabel = "time", ylabel = "flow rate [m³s⁻¹]")
     for link_metadata in values(model.integrator.p.graph.edge_data)
@@ -39,10 +40,8 @@ function Ribasim.plot_flow(model::Model; skip_conservative_out = true)
            link_metadata.link[1].type in Ribasim.conservative_nodetypes
             continue
         end
-        Ribasim.plot_flow!(model, ax, link_metadata)
+        plot_flow!(model, ax, link_metadata)
     end
     axislegend(ax)
     f
 end
-
-end # module RibasimMakieExt
