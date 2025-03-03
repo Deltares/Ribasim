4b89cc0bc76139f8b9176ba0dd9368312761c50e
Bart de Koning <bart123-4@hotmail.com>
2025-03-03 16:35:22 +0100

    Add test model

--- python/ribasim_testmodels/ribasim_testmodels/time.py
+++ python/ribasim_testmodels/ribasim_testmodels/time.py
@@ -2,7 +2,7 @@ import numpy as np
 import pandas as pd
 from ribasim.config import Experimental, Node
 from ribasim.model import Model
-from ribasim.nodes import basin, flow_boundary
+from ribasim.nodes import basin, flow_boundary, level_boundary, outlet, pump
 from shapely.geometry import Point
 
 
@@ -48,5 +48,40 @@ def flow_boundary_time_model() -> Model:
         model.flow_boundary[3],
         model.basin[2],
     )
+
+    return model
+
+
+def transient_pump_outlet_model() -> Model:
+    """Set up a model with time dependent pump and outlet flows."""
+    model = Model(
+        starttime="2020-01-01",
+        endtime="2021-01-01",
+        crs="EPSG:28992",
+        experimental=Experimental(concentration=True),
+    )
+
+    lb = model.level_boundary.add(
+        Node(1, Point(1, 0)), [level_boundary.Static(level=[1.0])]
+    )
+
+    time = ["2020-01-01", "2021-01-01"]
+    flow_rate = [0.0, 10.0]
+
+    out = model.outlet.add(
+        Node(2, Point(2, 0)), [outlet.Time(time=time, flow_rate=flow_rate)]
+    )
+
+    basin_data = [basin.State(level=[1.0]), basin.Profile(level=[0.0, 2.0], area=100.0)]
+
+    bsn1 = model.basin.add(Node(3, Point(3, 0)), basin_data)
+    bsn2 = model.basin.add(Node(5, Point(5, 0)), basin_data)
+
+    pmp = model.pmp.add(Node(4, 0), [pump.Time(time=time, flow_rate=flow_rate)])
+
+    model.link.add(lb, out)
+    model.link.add(out, bsn1)
+    model.link.add(bsn1, pmp)
+    model.link.add(pmp, bsn2)
 
     return model
