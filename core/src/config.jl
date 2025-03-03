5403fda6278a5b56e9aff127895b76b5115c4c79
Martijn Visser <mgvisser@gmail.com>
2025-03-03 15:05:36 +0100

    Oops

--- core/src/config.jl
+++ core/src/config.jl
@@ -191,7 +191,7 @@ end
 
 function Base.getproperty(config::Config, sym::Symbol)
     if sym === :dir
-        return getfield(config, :toml)
+        return getfield(config, :dir)
     else
         toml = getfield(config, :toml)
         return getproperty(toml, sym)
