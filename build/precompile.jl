2c881252294c287fe1754b5f7aea82460bab0cdb
Maarten Pronk <git@evetion.nl>
2025-03-04 09:25:54 +0100

    Use default SciML compilation settings.

--- build/precompile.jl
+++ build/precompile.jl
@@ -2,11 +2,12 @@
 # With the purpose of reducing the latency for compiled binaries.
 
 using Preferences, UUIDs
-set_preferences!(
-    UUID("0bca4576-84f4-4d90-8ffe-ffa030f20462"),  # SciMLBase
-    "SpecializationLevel" => "FullSpecialize";
-    force = true,
-)
+# Commented out due to segfault on building
+# set_preferences!(
+#     UUID("0bca4576-84f4-4d90-8ffe-ffa030f20462"),  # SciMLBase
+#     "SpecializationLevel" => "FullSpecialize";
+#     force = true,
+# )
 set_preferences!(
     UUID("aac5e3d9-0b8f-4d4f-8241-b1a7a9632635"),  # Ribasim
     "precompile_workload" => true;
@@ -19,11 +20,12 @@ toml_path = normpath(@__DIR__, "../generated_testmodels/basic/ribasim.toml")
 @assert Ribasim.main(toml_path) == 0
 
 # Remove preferences to avoid affecting normal Ribasim usage
-set_preferences!(
-    UUID("0bca4576-84f4-4d90-8ffe-ffa030f20462"),  # SciMLBase
-    "SpecializationLevel" => missing;
-    force = true,
-)
+# Commented out due to segfault on building
+# set_preferences!(
+#     UUID("0bca4576-84f4-4d90-8ffe-ffa030f20462"),  # SciMLBase
+#     "SpecializationLevel" => missing;
+#     force = true,
+# )
 set_preferences!(
     UUID("aac5e3d9-0b8f-4d4f-8241-b1a7a9632635"),  # Ribasim
     "precompile_workload" => missing;
