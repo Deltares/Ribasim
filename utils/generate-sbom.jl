37e34663710a093f25225a83428330761646603d
deltamarnix <deltamarnix@users.noreply.github.com>
2025-05-30 08:56:01 +0200

    Variable renames in script

--- utils/generate-sbom.jl
+++ utils/generate-sbom.jl
@@ -4,25 +4,25 @@ using Pkg
 
 Pkg.activate("core")
 
-myLicense = SpdxLicenseExpressionV2("MIT")
-myOrg = SpdxCreatorV2("Organization", "Deltares", "software@deltares.nl")
-myPackage_instr = spdxPackageInstructions(;
+ribasimLicense = SpdxLicenseExpressionV2("MIT")
+organization = SpdxCreatorV2("Organization", "Deltares", "software@deltares.nl")
+packageInstructions = spdxPackageInstructions(;
     spdxfile_toexclude = ["Ribasim.spdx.json"],
-    originator = myOrg,  # Could be myOrg if appropriate
-    declaredLicense = myLicense,
+    originator = organization,
+    declaredLicense = ribasimLicense,
     copyright = "Copyright (c) 2025 Deltares <software@deltares.nl>",
     name = "Ribasim",
 )
 
-active_pkgs = Pkg.project().dependencies;
+dependencies = Pkg.project().dependencies;
 spdxData = spdxCreationData(;
     Name = "Ribasim.jl",
-    Creators = [myOrg],
+    Creators = [organization],
     NamespaceURL = "https://github.com/Deltares/Ribasim/Ribasim.spdx.json",
-    rootpackages = active_pkgs,
+    rootpackages = dependencies,
     find_artifactsource = true,
     packageInstructions = Dict{UUID, spdxPackageInstructions}(
-        Pkg.project().uuid => myPackage_instr,
+        Pkg.project().uuid => packageInstructions,
     ),
 )
 
