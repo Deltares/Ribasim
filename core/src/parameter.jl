7675bed0d84d3cff82e7e5767f5af1edb96824ab
Bart de Koning <74617371+SouthEndMusic@users.noreply.github.com>
2025-06-03 13:02:05 +0200

    Fix cyclic Basin forcing (#2313)
    Fixes #2298, fixes #2291
    
    Notes:
    - Refactored getting `tstops` from timeseries interpolations in type
    stable manner
    - Refactored callbacks so that now always all of them are created
    - Running 1000 year `cyclic_time` model now takes 15 seconds
    - Concentrations cannot yet be periodic
    (https://github.com/Deltares/Ribasim/issues/2105, is this documented?)
    - Fixed some unit formatting and added reduction factor for resistance
    nodes in docs

--- core/src/parameter.jl
+++ core/src/parameter.jl
@@ -1043,6 +1043,9 @@ the object itself is not.
     u_prev_saveat::Vector{Float64} = Float64[]
     # Node ID associated with each state
     node_id::Vector{NodeID} = NodeID[]
+    # Callback configurations
+    do_concentration::Bool
+    do_subgrid::Bool
 end
 
 """
