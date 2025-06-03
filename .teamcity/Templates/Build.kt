5f91832bf47c75f4b519c29f358c8ec7c925e8c4
Maarten Pronk <git@evetion.nl>
2025-06-03 08:47:24 +0200

    [TC] Try to use cache with --check-bounds=yes. (#2332)
    Only some builds use the cache on TC, so this removes spaces from the
    name (not allowed as indicated by trying to edit the cache feature in
    the GUI). It also seems to recompile a lot, let's add --check-bounds=yes
    to the instantiate (used in cache creation) to try to prevent it.

--- .teamcity/Templates/Build.kt
+++ .teamcity/Templates/Build.kt
@@ -37,7 +37,7 @@ open class Build(platformOs: String) : Template() {
         features {
             buildCache {
                 id = "Ribasim${platformOs}Cache"
-                name = "Ribasim ${platformOs} Cache"
+                name = "Ribasim${platformOs}Cache"
                 publish = false
             }
         }
