e28fae0099ab3bf5442f05fbe9dbb02fce93bf72
lexverheem <lex.verheem@deltares.nl>
2025-06-04 10:22:26 +0200

    edit test

--- core/test/run_models_test.jl
+++ core/test/run_models_test.jl
@@ -723,46 +723,15 @@ end
     @test success(model)
 end
 
-@testitem "init_basin_only_area" begin
-    toml_path =
-        normpath(@__DIR__, "../../generated_testmodels/basic_basin_only_area/ribasim.toml")
-    @test ispath(toml_path)
-    model = Ribasim.run(toml_path)
-    @test model isa Ribasim.Model
-    @test success(model)
-end
-
-@testitem "init_basin_both_area_and_storage" begin
-    toml_path = normpath(
-        @__DIR__,
-        "../../generated_testmodels/basic_basin_both_area_and_storage/ribasim.toml",
-    )
-    @test ispath(toml_path)
-    model = Ribasim.run(toml_path)
-    @test model isa Ribasim.Model
-    @test success(model)
-end
-
-@testitem "init_basin_both_area_and_storage" begin
-    toml_path = normpath(
-        @__DIR__,
-        "../../generated_testmodels/basic_basin_both_area_and_storage/ribasim.toml",
-    )
-    @test ispath(toml_path)
-    model = Ribasim.run(toml_path)
-    @test model isa Ribasim.Model
-    @test success(model)
-end
-
 @testitem "debug interpolated relations" begin
+    using Ribasim
+    using DelimitedFiles: readdlm
     toml_path = normpath(
         @__DIR__,
         "../../generated_testmodels/basic_basin_both_area_and_storage/ribasim.toml",
     )
-    @test ispath(toml_path)
+
     model = Ribasim.run(toml_path)
-    @test model isa Ribasim.Model
-    @test success(model)
 
     levels = [0.0, 1.0, 2.0, 3.0, 4.0, 5.0]
     areas = [3.142, 6.283, 9.425, 12.566, 15.708, 18.850]
@@ -770,8 +739,6 @@ end
 
     filename =
         joinpath(model.config.dir, model.config.results_dir, "basin_profile_node_1.csv")
-
-    using DelimitedFiles
 
     node_id = 1
     data = readdlm(filename, ','; header = true)[1]
