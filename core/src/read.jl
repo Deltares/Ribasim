d28a449a731e4a2edadd6bbe8f4439feefe38ea2
lexverheem <lex.verheem@deltares.nl>
2025-06-06 09:18:50 +0200

    move to write

--- core/src/read.jl
+++ core/src/read.jl
@@ -1936,31 +1936,3 @@ function interpolate_basin_profile!(basin::Basin, profiles::StructVector{BasinPr
 
     return areas, levels, storage, node_ids
 end
-
-function output_basin_profiles(
-    all_levels::Vector{Vector{Float64}},
-    all_areas::Vector{Vector{Float64}},
-    all_storage::Vector{Vector{Float64}},
-    all_node_ids::Vector{Int32},
-    dir::AbstractString,
-)::Nothing
-    # Flatten all data and add node_id column
-    n = sum(length.(all_levels))
-    level = Vector{Float64}(undef, n)
-    area = Vector{Float64}(undef, n)
-    storage = Vector{Float64}(undef, n)
-    node_id = Vector{Int32}(undef, n)
-    idx = 1
-    for (i, nid) in enumerate(all_node_ids)
-        len = length(all_levels[i])
-        level[idx:(idx + len - 1)] = all_levels[i]
-        area[idx:(idx + len - 1)] = all_areas[i]
-        storage[idx:(idx + len - 1)] = all_storage[i]
-        node_id[idx:(idx + len - 1)] .= nid
-        idx += len
-    end
-    data = (; node_id, level, area, storage)
-    filename = joinpath(dir, "basin_profiles.csv")
-    mkpath(dirname(filename))
-    DelimitedFiles.writedlm(filename, Tables.columntable(data), ',')
-end
