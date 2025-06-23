7eac21d722ba5a635f1a6294e6263685a4d6db2c
Bart de Koning <bart123-4@hotmail.com>
2025-06-23 16:05:50 +0200

    Test piecewise linear basin profile

--- core/src/allocation_util.jl
+++ core/src/allocation_util.jl
@@ -189,7 +189,7 @@ function parse_profile(
     storage_to_level::AbstractInterpolation,
     level_to_area::AbstractInterpolation,
     lowest_level;
-    n_samples_per_segment = 10,
+    n_samples_per_segment = 100,
 )
     n_segments = length(storage_to_level.u) - 1
     samples_storage_node = zeros(n_samples_per_segment * n_segments + 1)
