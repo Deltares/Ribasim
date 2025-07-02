7e32d6c283bd9c6757fa3dd6f010023508614d2b
lexverheem <lex.verheem@deltares.nl>
2025-07-02 14:02:30 +0200

    remove plot

--- core/test/allocation_physics_test.jl
+++ core/test/allocation_physics_test.jl
@@ -98,18 +98,5 @@ end
     filter!(:link_id => ==(1), allocation_flow_table)
     filter!(:link_id => ==(1), flow_table)
 
-    using Plots
-
-    plot(
-        allocation_flow_table.time,
-        allocation_flow_table.flow_rate;
-        label = "Allocation Flow Rate",
-        xlabel = "Time",
-        ylabel = "Flow Rate",
-        legend = :topright,
-    )
-    plot!(flow_table.time, flow_table.flow_rate; label = "Flow Table Rate")
-    display(current())
-
     @test allocation_flow_table.flow_rate ≈ flow_table.flow_rate rtol = 1e-2
 end
