f66bccd7882d431b71e545db6bdba394e7a9c2b3
Martijn Visser <mgvisser@gmail.com>
2025-07-11 11:08:51 +0200

    Use pandera v0.25, and revert using pyarrow dtypes (#2450)
    This fixes #2120, we can update to the latest Pydantic release (0.25)
    again. There is still one open bug
    https://github.com/unionai-oss/pandera/issues/2016. That triggered me to
    essentially revert #1781 and switch back to the default pandas dtypes,
    because with them we are less likely to hit various compatibility issues
    in the ecosystem.
    
    Because we like nullable strings, integers and Booleans, we do use these
    now:
    - Nullable integer data type:
    https://pandas.pydata.org/docs/user_guide/integer_na.html
    - Nullable Boolean data type:
    https://pandas.pydata.org/docs/user_guide/boolean.html
    - StringDtype: https://pandas.pydata.org/docs/user_guide/text.html
    
    Furthermore this gets rid of Series in the type annotations using
    https://pandera.readthedocs.io/en/stable/dataframe_models.html#using-data-types-directly-for-column-type-annotations.
    This was needed because otherwise MyPy would complain about not allowing
    certain dtypes, but it is cleaner anyway.
    
    This also switches back to the default Timestamp with ns precision for
    the same reason. Possibly we need to support other units in the future
    if we do very long simulations.
    
    ---------
    
    Co-authored-by: Maarten Pronk <git@evetion.nl>

--- python/ribasim/ribasim/delwaq/generate.py
+++ python/ribasim/ribasim/delwaq/generate.py
@@ -73,18 +73,17 @@ def _make_boundary(data, boundary_type):
     """
     bid = _boundary_name(data.node_id.iloc[0], boundary_type)
     piv = (
-        data.pivot_table(
-            index="time", columns="substance", values="concentration", fill_value=-999
-        )
+        data.pivot_table(index="time", columns="substance", values="concentration")
         .reset_index()
         .reset_index(drop=True)
     )
-    # Convert Arrow time to Numpy to avoid needing tzdata somehow
-    piv.time = piv.time.astype("datetime64[ns]").dt.strftime("%Y/%m/%d-%H:%M:%S")
+    piv.time = piv.time.dt.strftime("%Y/%m/%d-%H:%M:%S")
     boundary = {
         "name": bid,
         "substances": list(map(_quote, piv.columns[1:])),
-        "df": piv.to_string(formatters={"time": _quote}, header=False, index=False),
+        "df": piv.to_string(
+            formatters={"time": _quote}, header=False, index=False, na_rep=-999
+        ),
     }
     substances = data.substance.unique()
     return boundary, substances
@@ -304,12 +303,8 @@ def generate(
     results_folder = toml_path.parent / model.results_dir
     evaporate_mass = model.solver.evaporate_mass
 
-    basins = pd.read_feather(
-        toml_path.parent / results_folder / "basin.arrow", dtype_backend="pyarrow"
-    )
-    flows = pd.read_feather(
-        toml_path.parent / results_folder / "flow.arrow", dtype_backend="pyarrow"
-    )
+    basins = pd.read_feather(toml_path.parent / results_folder / "basin.arrow")
+    flows = pd.read_feather(toml_path.parent / results_folder / "flow.arrow")
 
     output_path.mkdir(exist_ok=True)
 
