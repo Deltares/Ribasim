21fb2968580a4b3835b2b535c6ab09df79202d03
lexverheem <lex.verheem@deltares.nl>
2025-07-15 09:38:46 +0200

    Extend documentation

--- core/test/allocation_physics_test.jl
+++ core/test/allocation_physics_test.jl
@@ -56,7 +56,14 @@ end
     filter!(:link_id => ==(1), allocation_flow_table)
     filter!(:link_id => ==(1), flow_table)
 
-    @test allocation_flow_table.flow_rate ≈ flow_table.flow_rate rtol = 1e-2
+    # @test allocation_flow_table.flow_rate ≈ flow_table.flow_rate rtol = 1e-2
+
+    using Plots
+
+    plot(allocation_flow_table.time, allocation_flow_table.flow_rate; label = "allocation")
+    plot!(flow_table.time, flow_table.flow_rate; label = "flow", linestyle = :dash)
+    xlabel!("Time")
+    ylabel!("Flow rate")
 end
 
 @testitem "Outlet" begin
