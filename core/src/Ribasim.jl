fae6cf4cabf660f9ec24a84044aa049d3a7c1649
Martijn Visser <mgvisser@gmail.com>
2025-10-10 13:48:59 +0200

    Move out libribasim

--- core/src/Ribasim.jl
+++ core/src/Ribasim.jl
@@ -160,8 +160,6 @@ using Printf: @sprintf
 
 using Base.Threads: @threads
 
-export libribasim
-
 include("carrays.jl")
 using .CArrays: CVector, getaxes, getdata
 include("schema.jl")
@@ -183,7 +181,6 @@ include("bmi.jl")
 include("callback.jl")
 include("concentration.jl")
 include("main.jl")
-include("libribasim.jl")
 
 @setup_workload begin
     toml_path = normpath(@__DIR__, "../../generated_testmodels/basic/ribasim.toml")
