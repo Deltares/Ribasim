ee8e575c9fca79d22a327992dd9de06e5eba1364
Martijn Visser <mgvisser@gmail.com>
2025-10-17 10:12:59 +0200

    Don't specialize on build, take 2
    Replaces https://github.com/Deltares/Ribasim/pull/2643

--- build/precompile.jl
+++ build/precompile.jl
@@ -6,7 +6,7 @@ using Preferences, UUIDs
 uuid = UUID("aac5e3d9-0b8f-4d4f-8241-b1a7a9632635")  # Ribasim
 
 set_preferences!(uuid, "precompile_workload" => true; force = true)
-set_preferences!(uuid, "specialize" => true; force = true)
+set_preferences!(uuid, "specialize" => false; force = true)
 using Ribasim
 
 toml_path = normpath(@__DIR__, "../generated_testmodels/basic/ribasim.toml")
