3da0817de85cae0c870924a26b502113ad3aa73d
Martijn Visser <mgvisser@gmail.com>
2025-12-05 14:02:34 +0100

    Use Julia 1.12, Pkg workspaces and JuliaC sans trimming, take 2
    Trying https://github.com/Deltares/Ribasim/pull/2629 again now that we are some patch releases further along.

--- python/ribasim_api/tests/conftest.py
+++ python/ribasim_api/tests/conftest.py
@@ -6,7 +6,6 @@ import ribasim
 from ribasim_api import RibasimApi
 from ribasim_testmodels import (
     basic_model,
-    basic_transient_model,
     leaky_bucket_model,
     two_basin_model,
     user_demand_model,
@@ -23,13 +22,6 @@ def libribasim_paths() -> tuple[Path, Path]:
     return lib_path, lib_folder
 
 
-@pytest.fixture(scope="session", autouse=True)
-def load_julia(libribasim_paths) -> None:
-    lib_path, lib_folder = libribasim_paths
-    libribasim = RibasimApi(lib_path, lib_folder)
-    libribasim.init_julia()
-
-
 @pytest.fixture(scope="function")
 def libribasim(libribasim_paths, request) -> RibasimApi:
     lib_path, lib_folder = libribasim_paths
@@ -44,11 +36,6 @@ def libribasim(libribasim_paths, request) -> RibasimApi:
 @pytest.fixture(scope="session")
 def basic() -> ribasim.Model:
     return basic_model()
-
-
-@pytest.fixture(scope="session")
-def basic_transient(basic) -> ribasim.Model:
-    return basic_transient_model(basic)
 
 
 @pytest.fixture(scope="session")
