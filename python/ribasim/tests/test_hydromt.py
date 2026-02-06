67cfaa2c5708c5961c21eded6ac133ec70aa4581
Maarten Pronk <git@evetion.nl>
2026-02-06 13:24:36 +0100

    Also commit the tests.

---
+++ python/ribasim/tests/test_hydromt.py
@@ -0,0 +1,89 @@
+from pathlib import Path
+
+from ribasim.model import Model
+
+# HydroMT needs to be able to read and write partial Ribasim models.
+# That means, only reading the toml, only reading the geopackage, or only reading the netcdf forcing data.
+# The same applies for writing.
+
+# File reading is done recursively through the pydantic model structure.
+
+
+def test_basic_components(basic_arrow, tmp_path):
+    """Tests for hydromt input/output functionality.
+
+    Ensures that we can write individual components of a Ribasim model.
+    """
+    # A model with the profile table as external arrow file
+    model = basic_arrow
+
+    toml_path = tmp_path / "ribasim.toml"
+    db_path = tmp_path / "input" / "database.gpkg"
+    arrow_path = tmp_path / "input" / "profile.arrow"
+    assert model.basin.profile.filepath == Path(arrow_path.name)
+
+    # Saves only the toml file
+    model._write_toml(toml_path)
+    assert toml_path.exists()
+    assert not db_path.exists()
+    assert not arrow_path.exists()
+    toml_path.unlink()
+
+    # Saves only geopackage files without time time series data
+    model._save(tmp_path / model.input_dir, external=False)
+    assert db_path.exists()
+    assert not toml_path.exists()
+    assert not arrow_path.exists()
+    db_path.unlink()
+
+    # Saves only forcing files with time series data
+    model._save(tmp_path / model.input_dir, internal=False)
+    assert not db_path.exists()
+    assert not toml_path.exists()
+    assert arrow_path.exists()
+    arrow_path.unlink()
+
+    # Saves individual forcing table (arrow, netcdf, etc.)
+    model.basin.profile._save(tmp_path / "input")
+    assert not toml_path.exists()
+    assert not db_path.exists()
+    assert arrow_path.exists()
+    arrow_path.unlink()
+
+    # Saves individual non-forcing table (gpkg)
+    model.basin.static._save(tmp_path / "input")
+    assert not toml_path.exists()
+    assert db_path.exists()
+    assert not arrow_path.exists()
+    db_path.unlink()
+
+
+def test_basic_lazy_load(basic, tmp_path):
+    # Lazy model read yields only a config
+    # with empty spatial (node/link tables) and no other tables
+    model = Model.read("generated_testmodels/basic/ribasim.toml", lazy=True)
+
+    assert len(model.node_table().df) == 0
+    assert len(model.link.df) == 0  # Link Table is always initialized
+    assert model.basin.static.df is None
+    assert model.basin.static.lazy
+
+    assert model.starttime == basic.starttime
+    assert model.basin.time.df is None
+    assert model.basin.time.lazy
+
+    # Writing this lazy model is possible and yields the same
+    model.write(tmp_path / "ribasim.toml")
+    new_model = Model.read(tmp_path / "ribasim.toml")
+    assert new_model.basin.static.df is None
+    assert len(new_model.node_table().df) == 0
+    assert len(new_model.link.df) == 0
+
+    # We can load individual tables from the lazy model
+    model = Model.read("generated_testmodels/basic/ribasim.toml", lazy=True)
+    # The directory path is retrieved from the model
+    assert model.basin.root == model
+    assert model.basin.static.root == model
+    model.basin.static.load()
+    assert model.basin.static.df is not None
+    assert not model.basin.static.lazy
