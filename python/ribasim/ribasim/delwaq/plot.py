3fdc2bb56a4f68571286c6eaf66c85f30b33e15e
Martijn Visser <mgvisser@gmail.com>
2026-04-03 22:19:19 +0200

    Let stackplot handle the iterable
    Let's see if this fixes the docs built giving
    
    ERROR: RangeError: Maximum call stack size exceeded

--- python/ribasim/ribasim/delwaq/plot.py
+++ python/ribasim/ribasim/delwaq/plot.py
@@ -39,7 +39,7 @@ def plot_fraction(
     time = groups.get_group(key)["time"]
     ax.stackplot(
         time,
-        *stack.values(),
+        stack.values(),  # pyrefly: ignore[bad-argument-type]
         labels=stack.keys(),
     )
     ax.plot(
@@ -79,7 +79,8 @@ def plot_spatial(model, tracer="Initial", versus=None, limit=0.001, ax=None):
         alpha = c > limit
     else:
         total_concentration = (
-            table["concentration"][nodes.index] + vtable["concentration"][nodes.index]  # pyrefly: ignore[unbound-name]
+            # pyrefly: ignore[unbound-name]
+            table["concentration"][nodes.index] + vtable["concentration"][nodes.index]
         )
         c = table["concentration"][nodes.index] / total_concentration
         alpha = total_concentration / 2
