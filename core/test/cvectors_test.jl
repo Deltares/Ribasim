3b00397003164e12c115d4fc27f2a6c6188f739b
Bart de Koning <bart123-4@hotmail.com>
2026-07-10 16:34:39 +0200

    Better CArrays -> CVectors fix

--- core/test/cvectors_test.jl
+++ core/test/cvectors_test.jl
@@ -1,8 +1,8 @@
 @testitem "UnitRange" begin
-    using Ribasim.CArrays: CArray, CVector, getdata, getaxes
+    using Ribasim.CVectors: CVector, getdata, getaxes
     data = [1.0, 2.0, 3.0]
     axes = (a = 1:1, b = 2:3)
-    x = CArray(data, axes)
+    x = CVector(data, axes)
     @test x isa CVector{Float64}
     @test x isa DenseVector{Float64}
     @test length(x) == 3
@@ -10,7 +10,6 @@
     @test x[1] == 1.0
     @test x[2] == 2.0
     @test x[3] == 3.0
-    @test x[1:2] == [1.0, 2.0]
     @test x.a isa SubArray
     @test x.b isa SubArray
     @test getdata(x) === data
@@ -29,10 +28,10 @@
 end
 
 @testitem "Int" begin
-    using Ribasim.CArrays: CArray, CVector
+    using Ribasim.CVectors: CVector
     data = [1.0, 2.0, 3.0]
     axes = (a = 1, b = 2:3)
-    x = CArray(data, axes)
+    x = CVector(data, axes)
     @test x.a === 1.0
     @test x.b isa SubArray
     @test x.b == [2.0, 3.0]
@@ -42,49 +41,48 @@ end
 end
 
 @testitem "Nested" begin
-    using Ribasim.CArrays: CArray, CVector, getdata, getaxes
+    using Ribasim.CVectors: CVector, getdata, getaxes
     data = [1.0, 2.0, 3.0]
     axes = (; a = (; b = 1, c = 2:3))
-    x = CArray(data, axes)
+    x = CVector(data, axes)
     xa = x.a
     @test xa isa CVector
     @test length(xa) == 3
-    @test getaxes(xa) == (; b = 1, c = 2:3)
+    @test getdata(xa) === data
+    @test getaxes(xa) === axes.a
     @test_throws ErrorException x.b
     @test x.a.b === 1.0
     @test x.a.c isa SubArray
     @test x.a.c == [2.0, 3.0]
     @inferred getproperty(x, :a)
-    FloatView = SubArray{Float64, 1, SubArray{Float64, 1, Vector{Float64}, Tuple{UnitRange{Int64}}, true}, Tuple{UnitRange{Int64}}, true}
+    FloatView = SubArray{Float64, 1, Vector{Float64}, Tuple{UnitRange{Int64}}, true}
     @inferred Union{Float64, FloatView} getproperty(xa, :b)
     @inferred Union{Float64, FloatView} getproperty(xa, :c)
 
     # Test that nested axes with offset ranges have correct length and values
     data2 = collect(1.0:10.0)
     axes2 = (; foo = 1:5, bar = (; a = 6:7, b = 8:10))
-    v = CArray(data2, axes2)
+    v = CVector(data2, axes2)
     @test length(v.foo) == 5
     @test v.foo == [1.0, 2.0, 3.0, 4.0, 5.0]
     @test length(v.bar) == 5
     @test v.bar isa CVector
-    @test getdata(v.bar) == [6.0, 7.0, 8.0, 9.0, 10.0]
-    @test getaxes(v.bar) == (; a = 1:2, b = 3:5)
+    @test getdata(v.bar) === data2
+    @test getaxes(v.bar) === axes2.bar
     @test v.bar.a == [6.0, 7.0]
     @test v.bar.b == [8.0, 9.0, 10.0]
     @test length(v.bar.a) == 2
     @test length(v.bar.b) == 3
+    # Offset-aware indexing
+    @test v.bar[1] == 6.0
+    @test v.bar[5] == 10.0
 end
 
-@testitem "CMatrix" begin
-    using Ribasim.CArrays: CArray, CMatrix
-    data = [1.0; 2; 3;; 4; 5; 6]
-    axes = (a = 1, b = 2, c = CartesianIndex(3, 2))
-    x = CArray(data, axes)
-    x isa CMatrix
-    x.a === 1.0
-    x.b === 2.0
-    x.c === 6.0
-    @inferred getproperty(x, :a)
-    @inferred getproperty(x, :b)
-    @inferred getproperty(x, :c)
+@testitem "Contiguity" begin
+    using Ribasim.CVectors: CVector
+    data = [1.0, 2.0, 3.0, 4.0, 5.0]
+    # Gap between 1:2 and 4:5
+    @test_throws AssertionError CVector(data, (; a = 1:2, b = 4:5))
+    # Overlap between 1:3 and 3:5
+    @test_throws AssertionError CVector(data, (; a = 1:3, b = 3:5))
 end
