cfe2a04177496d1d22f023542e18ba4b715ab3fc
Bart de Koning <bart123-4@hotmail.com>
2026-08-06 16:12:41 +0200

    Fix CVector performance

--- core/src/cvectors.jl
+++ core/src/cvectors.jl
@@ -31,6 +31,11 @@ struct CVector{T, A <: DenseVector{T}, NT} <: DenseVector{T}
         offset = first(range) - 1
         return new{T, A, NT}(data, axes, offset, len)
     end
+
+    # Unchecked constructor for sub-CVectors where axes are already validated
+    function CVector(data::A, axes::NT, offset::Int, len::Int) where {T, A <: DenseVector{T}, NT <: NamedTuple}
+        return new{T, A, NT}(data, axes, offset, len)
+    end
 end
 
 function CVector{T, A, NT}(
@@ -156,9 +161,23 @@ end
 
 component(data, loc::Integer) = data[loc]
 component(data, loc::AbstractUnitRange{<:Integer}) = view(data, loc)
-component(data, loc::NamedTuple) = CVector(data, loc)
+@inline function component(data, loc::NamedTuple)
+    offset = _first_index(loc) - 1
+    len = _total_length(loc)
+    return CVector(data, loc, offset, len)
+end
 
-function Base.getproperty(x::CVector, name::Symbol)
+# Fast first/last index for nested NamedTuples without mapreduce overhead
+_first_index(loc::AbstractUnitRange{<:Integer}) = first(loc)
+_first_index(loc::NamedTuple) = _first_index(first(values(loc)))
+
+_last_index(loc::AbstractUnitRange{<:Integer}) = last(loc)
+_last_index(loc::NamedTuple) = _last_index(last(values(loc)))
+
+_total_length(loc::AbstractUnitRange{<:Integer}) = length(loc)
+_total_length(loc::NamedTuple) = _last_index(loc) - _first_index(loc) + 1
+
+Base.@constprop :aggressive @inline function Base.getproperty(x::CVector, name::Symbol)
     data = getdata(x)
     axes = getaxes(x)
     loc = getproperty(axes, name)
