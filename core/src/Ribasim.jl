5b9d215e21cd5343c2d8922c187273bc29da943a
Martijn Visser <mgvisser@gmail.com>
2026-08-16 21:04:30 +0200

    `get_du` is from SciMLBase

--- core/src/Ribasim.jl
+++ core/src/Ribasim.jl
@@ -32,7 +32,7 @@ using DifferentiationInterface:
 using ForwardDiff: derivative as forward_diff
 
 # Algorithms for solving ODEs.
-using OrdinaryDiffEqCore: OrdinaryDiffEqCore, get_du
+using OrdinaryDiffEqCore: OrdinaryDiffEqCore
 using OrdinaryDiffEqDifferentiation:
     OrdinaryDiffEqDifferentiation, dolinsolve, jacobian2W!
 using SciMLOperators: WOperator, MatrixOperator
@@ -50,6 +50,7 @@ using SciMLBase:
     CallbackSet,
     ODEFunction,
     ODEProblem,
+    get_du,
     get_proposed_dt,
     DEIntegrator,
     FullSpecialize,
